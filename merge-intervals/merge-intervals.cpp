628727a4c9670cb5ec2f02f45315a096c5b20e80
Suvraneel Bhuin <63473496+Suvraneel@users.noreply.github.com>
2021-12-26 02:24:34 +0530

    Time: 20 ms (59.69%), Space: 14.3 MB (40.04%) - LeetHub

---
+++ merge-intervals/merge-intervals.cpp
@@ -0,0 +1,22 @@
+	class Solution {
+public:
+    // Note - The optimal solution is to use an Interval Tree Data Structure
+
+    // Brute Force
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> res;
+        if (intervals.size() == 1) return {intervals[0]};
+        for(int i=1; i<intervals.size(); i++){
+            cout << intervals[i-1][0] << " " << intervals[i-1][1] << " | ";
+            if(intervals[i-1][1]>=intervals[i][0]){
+                intervals[i][0] = intervals[i-1][0];
+                intervals[i][1] = max(intervals[i-1][1],intervals[i][1]);
+                continue;
+            }
+            res.push_back(intervals[i-1]);
+        }
+        res.push_back(intervals[intervals.size()-1]);
+        return res;
+    }
+};
