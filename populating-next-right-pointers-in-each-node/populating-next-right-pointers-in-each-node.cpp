50c1b1e32160e5cad81cd0257926afd11e012253
Suvraneel Bhuin <63473496+Suvraneel@users.noreply.github.com>
2021-12-29 16:47:11 +0530

    Time: 33 ms (7.17%), Space: 17.9 MB (5.73%) - LeetHub

--- populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -18,13 +18,14 @@ public:
 
 class Solution {
 public:
-    vector<queue <Node*>> v;
+    vector<queue <Node*>> v;    // Vector of Queues
     bool firstTime = true;  // Meme reference : https://knowyourmeme.com/memes/james-franco-first-time
     Node* connect(Node* root, int lvl=0) {
         // Execute in first run only
         if (firstTime){
             if (!(root))
                 return root;
+            // Constructing empty queues for every level in tree
             auto ptr = root;
             while(ptr->left){
                 v.push_back(queue<Node*>());
@@ -32,18 +33,20 @@ public:
             }
             firstTime = false;
         }
-        if (root->left){
-            root->left->next = root->right;
-            if (!(v[lvl].empty())){//This auto-handles tree's left-most border `/`
-            (v[lvl].front())->next = root->left;
+        auto rootsLeft = root->left;
+        auto rootsRight = root->right;
+        if (rootsLeft){
+            rootsLeft->next = rootsRight;
+            if (!(v[lvl].empty())){// leftmost nodes are not the next of any node
+            (v[lvl].front())->next = rootsLeft;
             v[lvl].pop();
             }
-            connect(root->left, lvl+1);
+            connect(rootsLeft, lvl+1);
         }
-        if (root->right){
-            //dont push tree's right-most border `\`(Their next must remain null)
-            v[lvl].push(root->right);
-            connect(root->right, lvl+1);
+        if (rootsRight){
+            // next of tree's right-most border remains null
+            v[lvl].push(rootsRight);
+            connect(rootsRight, lvl+1);
         }
         return root;
     }
