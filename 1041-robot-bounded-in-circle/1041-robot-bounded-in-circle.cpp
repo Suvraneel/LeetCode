9dc54c84fdbf4bfccf1d375589301896b618d1b8
Suvraneel Bhuin <63473496+Suvraneel@users.noreply.github.com>
2022-01-09 15:23:57 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (79.18%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        array <int, 4> directn; //NESW
+        for (int i=0; i<4; i++)
+            directn[i]=0;
+        int facing = 0;
+        for(int k=0; k<4; k++){            
+            for (int i=0; i< instructions.length(); i++){
+                if(instructions[i]=='G')
+                    directn[facing]++;
+                else if(instructions[i]=='R')
+                    facing++;
+                else if(instructions[i]=='L')
+                    facing--;
+                if (facing>=4)
+                    facing %= 4;
+                else if(facing<0)
+                    facing += 4;
+            }
+        }
+        cout << directn[0]<<directn[2]<<directn[1]<<directn[3];
+        return (directn[0]==directn[2] && directn[1]==directn[3]);
+    }
+};
