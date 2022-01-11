089eefe4345f846ec20051965ea40640e533c2b7
Suvraneel Bhuin <63473496+Suvraneel@users.noreply.github.com>
2022-01-12 01:32:44 +0530

    Time: 0 ms (100.00%), Space: 16.8 MB (43.32%) - LeetHub

---
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res = 0;
+    int bits=0;
+    int sumRootToLeaf(TreeNode* root) {
+        recur(root, 0);
+        return (res);
+    }
+    
+    void recur(TreeNode* node, int currVal){
+        if (!(node))
+            return;
+        currVal = (currVal << 1) + node->val;
+        if(!(node->right) && !(node->left)){
+            res += currVal;
+            cout << currVal << endl;
+            return;
+        }
+        recur(node->left, currVal);
+        recur(node->right, currVal);
+    }
+};
