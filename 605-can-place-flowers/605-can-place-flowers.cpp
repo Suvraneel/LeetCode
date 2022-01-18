4c051d8b81eee6a0e941781a7cdf9beb08248277
Suvraneel Bhuin <63473496+Suvraneel@users.noreply.github.com>
2022-01-19 01:18:33 +0530

    Time: 32 ms (17.42%), Space: 20.3 MB (69.47%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         if(!(n) || (!(flowerbed.size())))
-            return true;            
+            return true;
+                    
         flowerbed.push_back(0);
         if(!(flowerbed[0]|flowerbed[1])){
             flowerbed[0]=1;
             n--;
         }
+        
         for(int i=1; i<flowerbed.size()-1; i++){
             if(!(n))
                 break;
@@ -15,7 +17,6 @@ public:
                 continue;
             n--;
             flowerbed[i]=1;
-            cout << i;
         }
         return (!(n));
     }
