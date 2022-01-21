046e8fc6eeb1d21bc08871e8355a37f0ca4bead8
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-01-22 02:18:55 +0530

    Time: 76 ms (41.78%), Space: 49.9 MB (45.58%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int res = 0;
-        
+
         // impossible case
         for(int i=0; i<gas.size(); i++){
             res += gas[i] - cost[i];
         }
         if (res < 0)
             return -1;
-        
+
         // normal case
         res = gas[0];
         int ct = 0, idx = 0;
