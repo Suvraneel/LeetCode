ba40e8c84c82d5054b44844756d0e634ce1821ef
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-02-07 12:56:40 +0530

    Time: 13 ms (6.98%), Space: 7 MB (9.15%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -3,9 +3,8 @@ public:
     char findTheDifference(string s, string t) {
         map <char, int> ms, mt;
         for (int i=0; i < s.length(); i++)
-            ms[s[i]]++;
-        for (int i=0; i < t.length(); i++)
-            mt[t[i]]++;
+            ms[s[i]]++, mt[t[i]]++;
+        mt[t[t.length()-1]]++;
         for (auto i:mt)
             if(ms[i.first]<i.second)
                 return i.first;
