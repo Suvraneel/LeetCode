a5e1cd182be881ef10c8e83957affbd2427f1234
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-08-02 02:21:15 +0530

    Time: 7 ms (7.14%), Space: 8.6 MB (26.61%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
+    #define mp make_pair
     map <pair<int, int>, int> memo;
     int uniquePaths(int m, int n) {
         if(!m || !n) return 0;
         if (m==1 && n==1){
             return 1;
         }
-        int a = memo[{m-1, n}] ? memo[{m-1, n}] : uniquePaths(m-1, n);
-        memo[{m-1, n}] = a;
-        int b = memo[{m, n-1}] ? memo[{m, n-1}] : uniquePaths(m, n-1);
-        memo[{m, n-1}] = b;
-        return a+b;
+        if(!memo[mp(m-1, n)])
+            memo[mp(m-1, n)] = uniquePaths(m-1, n);
+        if(!memo[mp(m, n-1)])
+            memo[mp(m, n-1)] = uniquePaths(m, n-1);
+        return memo[mp(m-1, n)] + memo[mp(m, n-1)];
     }
 };
