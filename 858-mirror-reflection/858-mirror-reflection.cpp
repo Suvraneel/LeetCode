5399a79c73a4fb3782fe95020c6d41b66d3c5de1
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-08-04 17:49:52 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (90.91%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -3,8 +3,7 @@ public:
     int mirrorReflection(int p, int q) {
         int LCM = lcm(p, q);
         // => Satisfying Glass Box Corner at Point(LCM/p, LCM/q) => // ie, on top-rt corner of that box
-        int mirroredXcount = LCM/q-1, mirroredYcount = LCM/p-1;
-        bool isXMirrored = mirroredXcount%2, isYMirrored = mirroredYcount%2;
+        int isXMirrored = (LCM/q-1)%2, isYMirrored = (LCM/p-1)%2;   // (no. of boxes on each axis) %2
         return isXMirrored?2:isYMirrored?0:1;
     }
 };
