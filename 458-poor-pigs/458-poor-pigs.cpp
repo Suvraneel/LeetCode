92a4991a6a484f290b05af565509b5ef5636ac7c
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-08-07 03:32:18 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (22.55%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.cpp
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int poorPigs(int buckets, int poisonTime, int totalTime) {
-        return ceil(log(buckets) / log(totalTime / poisonTime + 1));
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        return ceil(log(buckets) / log(minutesToTest / minutesToDie + 1));
     }
 };
