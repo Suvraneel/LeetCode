542c2516d473566c602c6f56eedcbfc32e0b5bb4
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-08-15 21:15:35 +0530

    Time: 15 ms (62.96%), Space: 8.2 MB (21.24%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        map <char, int> m;
-        m['I'] = 1;
-        m['V'] = 5;
-        m['X'] = 10;
-        m['L'] = 50;
-        m['C'] = 100;
-        m['D'] = 500;
-        m['M'] = 1000;
+        map <char, int> m{{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
         int res=0,temp=0;
         for (int i=0; i<s.length(); i++){
             if (m[s[i]]>temp){
