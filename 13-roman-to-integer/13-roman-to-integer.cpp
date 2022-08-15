b978ddf3c9fa47e9c54e7f98e1bfdfbe0478fd89
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-08-15 21:18:43 +0530

    Time: 11 ms (77.18%), Space: 8.2 MB (21.24%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -4,13 +4,12 @@ public:
         map <char, int> m{{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
         int res=0,temp=0;
         for (int i=0; i<s.length(); i++){
-            int token = m[s[i]];
-            if (token>temp){
-                temp = token - temp;
+            if (m[s[i]]>temp){
+                temp = m[s[i]] - temp;
                 continue;
             }
             res += temp;
-            temp = token;
+            temp = m[s[i]];
         }
         res += temp;
         return res;
