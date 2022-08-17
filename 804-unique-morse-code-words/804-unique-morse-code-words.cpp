d329a7f9fbcedcbd0f8da8823497b7da3369b263
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-08-17 18:21:24 +0530

    Time: 3 ms (87.06%), Space: 8.4 MB (88.09%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -1,14 +1,5 @@
 class Solution {
 public:
-    unordered_set <string> s;
-    int uniqueMorseRepresentations(vector<string>& words) {
-        string a[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        for(auto w:words){
-            string token = "";
-            for(auto l:w)
-                token+=a[l-'a'];
-            s.insert(token);
-        }
-        return s.size();
+    unordered_set <string> s;int uniqueMorseRepresentations(vector<string>& words) {string a[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};for(auto w:words){string token = "";for(auto l:w)token+=a[l-'a'];s.insert(token);}return s.size();
     }
 };
