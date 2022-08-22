b73f1ccb88e299022753a650642b19a0bc8a06b2
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-08-22 13:48:33 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (6.02%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         if(n<=0) return false;
-        float a = log10(n)/log10(4);
-        return floor(a)==a;
+        return floor(log10(n)/log10(4))==(log10(n)/log10(4));
     }
 };
