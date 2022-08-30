de9730dd00767252ffb289f690f23785dc64bebc
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-08-30 14:26:02 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (92.76%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        vector<int> x(matrix[0].size(), 0);
-        vector<vector<int>>v(matrix.size(), x);
-        for(int i=0; i<v.size(); i++){
-            for (int j=0; j<v[0].size(); j++){
-                v[i][j] = matrix[j][i];
+        reverse(matrix.begin(), matrix.end());
+        for(int i=0; i<matrix.size(); i++){
+            for (int j=i+1; j<matrix[0].size(); j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
-            reverse(v[i].begin(), v[i].end());
         }
-        matrix = v;
     }
 };
