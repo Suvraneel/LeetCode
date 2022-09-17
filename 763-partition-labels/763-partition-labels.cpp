15dc55e0ec5df5798a92dec6362dce8343b39805
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-09-18 03:44:26 +0530

    Time: 12 ms (20.44%), Space: 6.8 MB (24.33%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -5,14 +5,14 @@ public:
         vector <int> v;
         for(int i=0; i<s.size(); i++)
             m[s[i]]=i;
-        int idx=m[s[0]], prev=-1;
+        int streak=m[s[0]], prev=-1;
         for(int i=0; i<s.size(); i++)
-            if(i==idx){
-                v.push_back(idx-prev);
-                prev = idx;
-                idx = i<s.size()-1 ? m[s[i+1]]:0;
+            if(i==streak){
+                v.push_back(streak-prev);
+                prev = streak;
+                streak = i<s.size()-1 ? m[s[i+1]]:0;
             }
-            else idx=max(m[s[i]], idx);
+            else streak=max(m[s[i]], streak);
         return v;
     }
 };
