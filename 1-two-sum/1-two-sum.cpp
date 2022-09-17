27ba4d9474a63771c231979fdf5227a7b50131f2
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-09-18 02:29:08 +0530

    Time: 37 ms (45.57%), Space: 11.9 MB (13.71%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map <int, int>  m;
+        vector<int> v;
+        v.resize(2, 0);
+        for (int i=0; i<nums.size(); i++){
+            if(m[target-nums[i]]){
+                v[0]=m[target-nums[i]]-1;   // since we stored as index+1
+                v[1]=i;
+                return v;
+            }
+            else m[nums[i]]=i+1;    //+1 bcz if we push index 0 to map, then it will be detected as null... So converting to 1-indexed array;
+        }
+        return v;
+    }
+};
