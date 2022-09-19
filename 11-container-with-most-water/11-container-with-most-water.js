3fc27d8a5a4484cc51a9b11279ca09faa61aa016
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-09-20 02:49:48 +0530

    Time: 157 ms (11.25%), Space: 49.4 MB (74.07%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(ht) {
+    var maxm=0, lt=0, rt=ht.length-1;
+    while(lt<rt){
+        maxm = Math.max(maxm,(rt-lt)*Math.min(ht[lt], ht[rt]));
+        if(ht[lt]<ht[rt])
+            lt++;
+        else rt--;
+    }
+    return maxm;
+};
