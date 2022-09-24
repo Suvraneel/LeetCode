c680a05374d10bb384e63dd22c5a03a5ada97b75
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-09-24 21:03:51 +0530

    Time: 30 ms (31.89%), Space: 19.9 MB (60.91%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -11,20 +11,23 @@
  */
 class Solution {
 public:
-    vector<vector<int>> res, z;
+    vector<vector<int>> res;
     vector <int> v;
-    vector<vector<int>> pathSum(TreeNode* root, int targetSum, int currSum=0) {
+    
+    void solve(TreeNode* root, int targetSum, int currSum) {
         if(!root)
-            return z;
+            return;
         v.push_back(root->val);
         currSum += root->val;
-        if(!root->left && !root->right){
-            if(targetSum==currSum)
+        if(!root->left && !root->right && targetSum==currSum)
                 res.push_back(v);
-        }
-        pathSum(root->left, targetSum, currSum);
-        pathSum(root->right, targetSum, currSum);
+        solve(root->left, targetSum, currSum);
+        solve(root->right, targetSum, currSum);
         v.pop_back();
+    }
+    
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        solve(root, targetSum, 0);
         return res;
     }
 };
