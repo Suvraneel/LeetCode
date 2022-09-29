b09c9fd49087cadb6a2ebb919fa02e03d66e3c41
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-09-29 16:51:40 +0530

    Time: 48 ms (91.78%), Space: 31 MB (98.27%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,21 +1,32 @@
+// class Solution {
+// public:
+//     struct comparator{
+//         int x;
+//         comparator(int x){
+//             this->x=x;
+//         }
+//         bool operator ()(int a, int b){
+//             return abs(a-x)<abs(b-x);
+//         }
+//     };
+    
+//     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+//         if(arr.size()==1) return arr;
+//         stable_sort(arr.begin(), arr.end(), comparator(x));
+//         arr=vector<int>(arr.begin(),arr.begin()+k);
+//         // arr.erase(arr.begin()+k, arr.end());
+//         sort(arr.begin(), arr.end());
+//         return arr;
+//     }
+// };
+
 class Solution {
 public:
-    struct comparator{
-        int x;
-        comparator(int x){
-            this->x=x;
-        }
-        bool operator ()(int a, int b){
-            return abs(a-x)<abs(b-x);
-        }
-    };
-    
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        if(arr.size()==1) return arr;
-        stable_sort(arr.begin(), arr.end(), comparator(x));
-        arr=vector<int>(arr.begin(),arr.begin()+k);
-        // arr.erase(arr.begin()+k, arr.end());
-        sort(arr.begin(), arr.end());
-        return arr;
+        if(arr.size()==1 || arr.size()==k) return arr;
+        int lt=0, rt=arr.size()-1;
+        while(rt-lt>=k)
+            abs(arr[lt]-x)>abs(arr[rt]-x)?lt++:rt--;
+        return vector<int>(arr.begin()+lt, arr.begin()+rt+1);
     }
 };
