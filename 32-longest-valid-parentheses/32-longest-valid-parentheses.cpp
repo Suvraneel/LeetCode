35ca213e4361d7800ad89b8562f3c56365a8df35
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-10-01 02:09:20 +0530

    Time: 8 ms (51.21%), Space: 7.4 MB (41.14%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -4,9 +4,8 @@ public:
         stack<int> stk;
         int ans=0;
         stk.push(-1);
-        for(int i=0;i<s.size();i++)
-        {
-            int t=stk.top();
+        for(int i=0;i<s.size();i++){
+            int t= stk.top();
             if(t!=-1 && s[i]==')' && s[t]=='(')
                 stk.pop(), ans=max(ans,i-stk.top());
             else
