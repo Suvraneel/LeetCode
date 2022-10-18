be84ce2df072cdd0498776c580d391234ad70961
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-10-18 11:20:31 +0530

    Time: 18 ms (47.09%), Space: 6.6 MB (77.40%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    string convert(string s){
-        string res="";
-        char prev=s[0];
-        int ct=0;
-        for(char c: s){
-            if(c==prev)
-                ct++;
-            else {
-                res+=to_string(ct)+to_string(prev-'0');
-                ct=1;
-                prev=c;
-            }
-        }
-        return res+to_string(ct)+to_string(prev-'0');
-    }
     string countAndSay(int n) {
         string ans="1";
-        for(int i=2; i<=n; i++)
-            ans=convert(ans);
+        for(int i=2; i<=n; i++){
+            string res="";
+            char prev=ans[0];
+            int ct=0;
+            for(char c: ans){
+                if(c==prev)
+                    ct++;
+                else {
+                    res+=to_string(ct)+to_string(prev-'0');
+                    ct=1;
+                    prev=c;
+                }
+            }
+            res+=to_string(ct)+to_string(prev-'0');
+            ans=res;
+        }
         return ans;
     }
 };
