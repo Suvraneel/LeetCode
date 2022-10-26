c407b35c78aa50bac04006ce70165e6f2a1a18b2
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-10-26 13:50:55 +0530

    Time: 614 ms (11.83%), Space: 113.7 MB (83.55%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int running_sum = 0;
+        for(int i=0; i<nums.size(); i++)
+            nums[i] = running_sum += nums[i];
+        unordered_map<int, int> seen;
+        // Based on the result that if SUM_JmodK==SUM_ImodK  => (SUM_J-SUM_I)modK==0
+        for(int i=0; i<nums.size(); i++){
+            int rem = nums[i]%k;
+            if(i>=1 && !rem)      // base case: contiguous subarray of all array elems upto index i
+                return true;
+            if(seen[rem]){
+                if(i+1-seen[rem]>=2)  // result found
+                    return true;
+                // else dont update array, cuz it doesnt hurt to find the largest satisfying array
+            }
+            else seen[rem] = i+1;     // first occurance -> store
+        }
+        return false;   // not possible
+    }
+};
