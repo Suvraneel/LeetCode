a5148b73eed853edf1b773b7cb52912b4a864b9b
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-11-01 16:20:56 +0530

    Time: 43 ms (71.17%), Space: 13.3 MB (52.18%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findBall(vector<vector<int>>& grid) {
+        vector<int> res;
+        for(int i=0; i<grid[0].size(); i++){
+            pair<int, int> curr = {0, i};
+            bool stuck = false;
+            while(curr.first!=grid.size()-1){
+                int currVal = grid[curr.first][curr.second];
+                if(curr.second+currVal<grid[0].size() && curr.second+currVal>=0 && currVal*grid[curr.first][curr.second+currVal]==1)
+                    curr.first++, curr.second+=currVal;
+                else {
+                    stuck = true; 
+                    break;
+                }
+                // cout << "(" << curr.first << "," << curr.second << ")\n";
+            }
+            // cout << "---" << endl;
+            int currVal = grid[curr.first][curr.second];
+            res.push_back(stuck || curr.second+currVal>=grid[0].size() || curr.second+currVal<0 || currVal*grid[curr.first][curr.second+currVal]!=1?-1:curr.second+grid[curr.first][curr.second]);
+        }
+        return res;
+    }
+};
