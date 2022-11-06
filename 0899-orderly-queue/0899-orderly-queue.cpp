e2c19664cc9691c48a83eb721918cbd230dc964e
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-11-06 11:27:21 +0530

    Time: 7 ms (63.22%), Space: 10.8 MB (52.30%) - LeetHub

--- 0899-orderly-queue/0899-orderly-queue.cpp
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     string orderlyQueue(string s, int k) {
-        if(k>1){
-            sort(s.begin(), s.end());
-            return s;
+        if(k==1){
+            string res= s;
+            for(int i=0; i<s.size(); i++){
+                res = min(res, s.substr(i)+s.substr(0,i));
+            }
+            return res;
         }
-        string res= s;
-        for(int i=0; i<s.size(); i++){
-            res = min(res, s.substr(i)+s.substr(0,i));
-        }
-        return res;
+        sort(s.begin(), s.end());
+        return s;
     }
 };
