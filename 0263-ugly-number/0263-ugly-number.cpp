ac742bdfd1edab090185e811a8407bb15cedf6e9
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-11-18 20:48:22 +0530

    Time: 3 ms (65.96%), Space: 6 MB (19.97%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        if(n<1)
-            return false;
-        while(n>1){
-            if(n%5==0)
-                n/=5;
-            else if(n%3==0)
-                n/=3;
-            else if(n%2==0)
-                n/=2;
-            else return false;
-        }
-        return true;
+        if(n<1) return false;
+        while(!(n%5)) n/=5;
+        while(!(n%3)) n/=3;
+        while(!(n%2)) n/=2;
+        return n==1;
     }
 };
