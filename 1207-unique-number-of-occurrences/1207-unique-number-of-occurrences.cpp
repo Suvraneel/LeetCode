191e030056c286cd38fa7365c5b326c4d50d0c52
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-11-30 12:05:30 +0530

    Time: 9 ms (26.75%), Space: 8.3 MB (20.75%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map <int, int> m;
+        unordered_map <int, int> m1, m2;
         unordered_set <int> s;
         for(auto e: arr)
-            m[e]++;
-        for(auto e: m)
-            s.insert(e.second);
-        return m.size()==s.size();
+            m1[e]++;
+        for(auto [K, V]: m1)
+            if(!m2[V])
+                m2[V]=K;
+            else return false;
+        return true;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool uniqueOccurrences(vector<int>& arr) {
+//         unordered_map <int, int> m;
+//         unordered_set <int> s;
+//         for(auto e: arr)
+//             m[e]++;
+//         for(auto e: m)
+//             s.insert(e.second);
+//         return m.size()==s.size();
+//     }
+// };
