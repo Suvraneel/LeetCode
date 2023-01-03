d7ffc3b13ace9cb54b120aa330216c53650bbdca
Suvraneel Bhuin <bsuvraneel@gmail.com>
2023-01-03 14:12:30 +0530

    Time: 65 ms (58.18%), Space: 12.2 MB (30.23%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
-        int res = 0;
-        vector<char> cols(strs[0].size(),'a');
-        vector<bool> del(strs[0].size(), false);
-        for(auto s:strs){
-            for(int i=0; i<cols.size();i++){
-                if(del[i])
-                    continue;
-                if(cols[i]>s[i]){
-                    res++;
-                    del[i]=true;
+        int count = 0;
+        int n = strs.size(), m = strs[0].size();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n-1; j++){
+                if(strs[j][i]>strs[j+1][i]){
+                    count++;
+                    break;
                 }
-                cols[i]=s[i];
             }
         }
-        return res;
+        return count;
     }
 };
