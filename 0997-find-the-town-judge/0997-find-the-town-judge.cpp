8705fedc231e7b7692d2688227550abae8f67050
Suvraneel Bhuin <bsuvraneel@gmail.com>
2023-01-23 14:17:25 +0530

    Time: 214 ms (63.42%), Space: 68.8 MB (42.03%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector <pair<int, bool>> v(n+1, {0, false}); // person => {trusted_by_ct, trusts_someone}
+        vector <int> v(n+1, 0); // person => {trusted_by_ct, trusts_someone}
         for(auto e: trust)
-            v[e[1]].first++, v[e[0]].second=true;
+            v[e[1]]++, v[e[0]]=INT_MIN;
         for(int i=1; i<v.size(); i++)
-            if(v[i].first==n-1 && !v[i].second)
+            if(v[i]==n-1)
                 return i;
         return -1;
     }
