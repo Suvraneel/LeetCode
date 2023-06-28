779e49b3725f37e2c5cbed3dd15960dfd02068cd
Suvraneel Bhuin <bsuvraneel@gmail.com>
2023-06-29 03:35:15 +0530

    Time: 636 ms (5.03%), Space: 70.3 MB (22.06%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-
-        // Adjacency list
-        vector<vector<pair<int, double>>> adj(n);
-        for (int i = 0; i < edges.size(); i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            adj[u].push_back({v, succProb[i]});
-            adj[v].push_back({u, succProb[i]});
+        vector<vector<pair<int, double>>> adjList(n);
+        int i = 0;
+        for(auto e: edges){
+            int u = e[0], v = e[1];
+            adjList[u].push_back({v, succProb[i]});
+            adjList[v].push_back({u, succProb[i++]});
         }
-
-        // ans will be in dist[end]
-        vector<double> dist(n, 0.0);
-        dist[start] = 1.0;
         
-        queue<int> q;
+        vector<double> prob(n, 0.0);    // cost to reach a node (here: probability)
+        prob[start] = 1.0;
+        queue <int> q;
         q.push(start);
-        
-        while (!q.empty()) {
-            int curr = q.front();
+        while (!q.empty()){
+            int cur = q.front();
             q.pop();
-            
-            for (auto x : adj[curr]) {
-                int node = x.first;
-                double prob = x.second;
-                double newProb = dist[curr] * prob;
-                
-                if (newProb > dist[node]) {
-                    dist[node] = newProb;
-                    q.push(node);
+            for(auto nbr: adjList[cur]){
+                double prob_new = prob[cur] * nbr.second;
+                if(prob_new > prob[nbr.first]){
+                    prob[nbr.first] = prob_new;
+                    cout << cur << "---" << nbr.first << "<-" << prob_new << "\n";
+                    q.push(nbr.first);
                 }
             }
         }
-        
-        return dist[end];
+        return prob[end];
     }
 };
