dd420243d86848e70d5693f9e53e44b52eae4c0a
Suvraneel Bhuin <bsuvraneel@gmail.com>
2023-09-15 22:34:58 +0530

    Time: 61 ms (96.98%), Space: 10.4 MB (92.71%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& pts) {
-        int n=pts.size(), cost=0, i=0, connected=0;
+        int n=pts.size(), cost=0, i=0;
         vector <int> d(n, INT_MAX-1);
-        while(++connected < n){
+        for(int connected=0; ++connected < n;){
             d[i]=INT_MAX;
             int nearest = i;
             for(int j=0; j<n; ++j){
-                if(d[j]!=INT_MAX){
+                if(d[j]!=INT_MAX){  // isConnected?
                     d[j]=min(d[j], abs(pts[j][0]-pts[i][0])+abs(pts[j][1]-pts[i][1]));
                     nearest = d[j]<d[nearest]?j:nearest;
                 }
