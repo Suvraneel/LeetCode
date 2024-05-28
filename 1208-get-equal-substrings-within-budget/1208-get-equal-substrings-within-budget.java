023210d59f731d70279281ffd46ca3910d3638c7
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-05-29 03:57:59 +0530

    Time: 5 ms (97.71%), Space: 43.2 MB (31.1%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
@@ -5,18 +5,12 @@ class Solution {
         int diff[] = new int[n];
         for (int i = 0; i < n; i++)
             diff[i] = Math.abs(s.charAt(i) - t.charAt(i));
-        // for (int i = 0; i < n && cost <= maxCost; i++) {
-        //     maxLen++;
-        //     cost += diff[i];
-        // }
         int lt = 0, rt = 0, cost = 0;
         while (rt < n) {
+            cost += diff[rt++];
             while (cost > maxCost)
                 cost -= diff[lt++];
-            while (rt < n && cost <= maxCost)
-                cost += diff[rt++];
-            maxLen = Math.max(maxLen, rt - lt - 1);
-            // System.out.println(maxLen);
+            maxLen = Math.max(maxLen, rt - lt);
         }
         return maxLen;
     }
