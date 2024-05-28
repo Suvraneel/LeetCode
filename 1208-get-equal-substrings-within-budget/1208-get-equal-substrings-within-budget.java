b4f8ab4da70ebeb7dae46f5742335c05c8488875
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-05-29 04:01:08 +0530

    Time: 5 ms (97.71%), Space: 43.4 MB (23.48%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
@@ -1,8 +1,8 @@
 class Solution {
     public int equalSubstring(String s, String t, int maxCost) {
-        int diff[] = new int[s.length()];
-        int lt = 0, rt = 0, cost = 0, maxLen = 0;
-        while (rt < s.length()) {
+        int lt = 0, rt = 0, cost = 0, maxLen = 0, n = s.length();
+        int diff[] = new int[n];
+        while (rt < n) {
             diff[rt] = Math.abs(s.charAt(rt) - t.charAt(rt));
             cost += diff[rt++];
             while (cost > maxCost)
