2a7fa37785ce675526f70c60fb6598131ef37840
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-06-07 03:12:37 +0530

    Time: 22 ms (95.82%), Space: 45.4 MB (51.51%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -2,23 +2,19 @@ class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
         if (hand.length % groupSize != 0)
             return false;
-        int nGrps = hand.length / groupSize;
         Map<Integer, Integer> freq = new HashMap<>();
         for (int i : hand)
             freq.put(i, freq.getOrDefault(i, 0) + 1);
-        for(int i: hand){
+        for (int i : hand) {
             int start = i;
-            while(freq.getOrDefault(start-1,0)!=0)   // find safe streak start
+            while (freq.getOrDefault(start - 1, 0) != 0) // find safe streak start
                 start--;
-            while(start<i){
-                while(freq.getOrDefault(start, 0)!=0){
-                    for(int curr = start; curr<start+groupSize; curr++){
-                        if(freq.getOrDefault(curr,0)==0)    // incomplete streak
-                            return false;
-                        freq.put(curr, freq.get(curr)-1);
-                    }
+            while (freq.getOrDefault(start, 0) != 0) {
+                for (int curr = start; curr < start + groupSize; curr++) {
+                    if (freq.getOrDefault(curr, 0) == 0) // incomplete streak
+                        return false;
+                    freq.put(curr, freq.get(curr) - 1);
                 }
-                start++;
             }
         }
         return true;
