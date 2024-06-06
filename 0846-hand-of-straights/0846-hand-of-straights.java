4e9e206ac6fc03a7796abab4582cb65bd6028c8b
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-06-07 03:03:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -1,28 +1,25 @@
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        if(hand.length%groupSize!=0)
+        if (hand.length % groupSize != 0)
             return false;
-        int nGrps = hand.length/groupSize;
-        Map<Integer, Integer> freq = new TreeMap<>();
-        for(int i:hand)
-            freq.put(i, freq.getOrDefault(i, 0)+1);
-        for(int i=0; i<nGrps; i++){
-            int grpSizeCt = 0, prev = -1;
-            for(Map.Entry<Integer, Integer> e: freq.entrySet()){
-                if(e.getValue()==0){
-                    continue;
+        int nGrps = hand.length / groupSize;
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int i : hand)
+            freq.put(i, freq.getOrDefault(i, 0) + 1);
+        for(int i: hand){
+            int start = i;
+            while(freq.getOrDefault(start-1,0)!=0)   // find safe streak start
+                start--;
+            while(start<i){
+                while(freq.getOrDefault(start, 0)!=0){
+                    for(int curr = start; curr<start+groupSize; curr++){
+                        if(freq.getOrDefault(curr,0)==0)    // incomplete streak
+                            return false;
+                        freq.put(curr, freq.get(curr)-1);
+                    }
                 }
-                if(prev==-1)
-                    prev = e.getKey();
-                else if(e.getKey()!=prev+1)
-                    return false;
-                e.setValue(e.getValue()-1);
-                prev = e.getKey();
-                if(++grpSizeCt==groupSize)
-                    break;
+                start++;
             }
-            if(grpSizeCt<groupSize)
-                return false;
         }
         return true;
     }
