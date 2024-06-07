8701f0f7f3cb6d47ea7f25270531df74433b3bf3
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-06-08 01:56:39 +0530

    Time: 21 ms (58.46%), Space: 55.3 MB (60.1%) - LeetHub

--- 0648-replace-words/0648-replace-words.java
+++ 0648-replace-words/0648-replace-words.java
@@ -1,12 +1,89 @@
 class Solution {
-    public String replaceWords(List<String> dictionary, String sentence) {
-        String[] sent = sentence.split(" ");
-        for(String word: dictionary){
-            for(int i=0; i<sent.length; i++){
-                if(sent[i].startsWith(word))
-                    sent[i] = word;
-            }
+    class TrieNode {
+        TrieNode[] children;
+        boolean isEndOfWord;
+
+        public TrieNode() {
+            this.children = new TrieNode[26];
+            this.isEndOfWord = false;
         }
-        return String.join(" ", sent);
+    }
+
+    class Trie {
+        private final TrieNode root;
+
+        public Trie() {
+            this.root = new TrieNode();
+        }
+
+        public void insert(String word) {
+            TrieNode node = root;
+            for (char c : word.toCharArray()) {
+                int idx = c - 'a';
+                if (node.children[idx] == null)
+                    node.children[idx] = new TrieNode();
+                node = node.children[idx];
+            }
+            node.isEndOfWord = true;
+        }
+
+        public boolean search(String word) {
+            TrieNode node = root;
+            for (char c : word.toCharArray()) {
+                int idx = c - 'a';
+                if (node.children[idx] == null)
+                    return false;
+                node = node.children[idx];
+            }
+            return node.isEndOfWord;
+        }
+
+        public boolean startsWith(String prefix) {
+            TrieNode node = root;
+            for (char c : prefix.toCharArray()) {
+                int idx = c - 'a';
+                if (node.children[idx] == null)
+                    return false;
+                node = node.children[idx];
+            }
+            return true;
+        }
+
+        public String findRoot(String word) {
+            TrieNode node = root;
+            String root = "";
+            for (char c : word.toCharArray()) {
+                int idx = c - 'a';
+                if (node.children[idx] == null)
+                    return null;
+                root += c;
+                node = node.children[idx];
+                if (node.isEndOfWord)
+                    return root;
+            }
+            return root;
+        }
+
+    }
+
+    /**
+     * Your Trie object will be instantiated and called as such:
+     * Trie obj = new Trie();
+     * obj.insert(word);
+     * boolean param_2 = obj.search(word);
+     * boolean param_3 = obj.startsWith(prefix);
+     */
+
+    public String replaceWords(List<String> dictionary, String sentence) {
+        String[] words = sentence.split(" ");
+        Trie dict = new Trie();
+        for(String word: dictionary)
+            dict.insert(word);
+        for(int i=0; i<words.length; i++){
+            String root = dict.findRoot(words[i]);
+            if(root!=null)
+                words[i] = root;
+        }
+        return String.join(" ", words);
     }
 }
