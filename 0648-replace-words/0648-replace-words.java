ce37bee68cc7cfa0ce42895ad123dfae694028bc
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-06-08 01:50:26 +0530

    Time: 29 ms (53.03%), Space: 51.4 MB (99.24%) - LeetHub

--- 0648-replace-words/0648-replace-words.java
+++ 0648-replace-words/0648-replace-words.java
@@ -1,7 +1,6 @@
 class Solution {
     public String replaceWords(List<String> dictionary, String sentence) {
         String[] sent = sentence.split(" ");
-        dictionary.sort(Comparator.comparingInt(String::length));
         for(String word: dictionary){
             for(int i=0; i<sent.length; i++){
                 if(sent[i].startsWith(word))
