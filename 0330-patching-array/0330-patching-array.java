3eb3ede00c6f551e47d0d8138b4934bcec4f22be
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-06-16 22:48:16 +0530

    Time: 0 ms (100%), Space: 43.9 MB (63.4%) - LeetHub

--- 0330-patching-array/0330-patching-array.java
+++ 0330-patching-array/0330-patching-array.java
@@ -1,30 +1,15 @@
-class Solution
-{
-public
-    int minPatches(int[] nums, int n)
-    {
-        long currentSum = 1; // Tracks the smallest number that cannot be formed
-        int patches = 0;     // Number of patches added
-        int index = 0;       // Index to iterate through the nums array
-
-        // Continue until we can form numbers up to n
-        while (currentSum <= n)
-        {
-            // If current index lies in the array 
-            //and the current value is less than equal to the currentSum
-            if (index < nums.length && nums[index] <= currentSum)
-            {
-                // If the current number in nums can be added to currentSum
-                currentSum += nums[index]; // Include nums[index] in the range
-                index++;
-            }
-            else
-            {
-                // Add a patch (currentSum itself) to cover the range up to currentSum * 2 - 1
-                currentSum += currentSum;
-                patches++;
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        int len = nums.length, res = 0, i = 0;
+        long missing = 1;
+        while (missing <= n) {
+            if (i < len && nums[i] <= missing) {
+                missing += nums[i++];
+            } else {
+                missing += missing;
+                res++;
             }
         }
-        return patches;
+        return res;
     }
 }
