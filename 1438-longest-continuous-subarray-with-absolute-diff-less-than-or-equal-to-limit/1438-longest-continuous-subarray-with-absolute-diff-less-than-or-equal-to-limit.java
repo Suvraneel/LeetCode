d0076ecf2a6c0c6ae2905de462c0f3006c4dda70
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-06-23 18:35:34 +0530

    Time: 81 ms (29.65%), Space: 57.4 MB (24.01%) - LeetHub

---
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -0,0 +1,62 @@
+class Solution {
+    private class DblPriorityQ {
+        TreeMap<Integer, Integer> pq;
+
+        public DblPriorityQ() {
+            pq = new TreeMap<>();
+        }
+
+        public void insert(int key) {
+            this.pq.put(key, this.pq.getOrDefault(key, 0) + 1);
+        }
+
+        public int getMin() {
+            return this.pq.firstEntry().getKey();
+        }
+
+        public int getMax() {
+            return this.pq.lastEntry().getKey();
+        }
+
+        public void remove(int key) {
+            if (!this.pq.containsKey(key))
+                return;
+            if (this.pq.get(key) > 1)
+                this.pq.put(key, this.pq.get(key) - 1);
+            else
+                this.pq.remove(key);
+        }
+
+        public int findAbsDiff() {
+            if (pq.isEmpty())
+                return 0;
+            return (getMax() - getMin());
+        }
+
+        public String toString() {
+            StringBuilder buffer = new StringBuilder();
+            for (Map.Entry<Integer, Integer> e : this.pq.entrySet())
+                for (int i = 0; i < e.getValue(); i++)
+                    buffer.append(e.getKey()).append(", ");
+            if (buffer.length() > 0)
+                buffer.setLength(buffer.length() - 2);
+            return buffer.toString();
+        }
+    }
+
+    public int longestSubarray(int[] nums, int limit) {
+        int n = nums.length, lt = 0, rt = 0, maxLen = 0;
+        DblPriorityQ pq = new DblPriorityQ();
+        while (rt < n) {
+            while (pq.findAbsDiff() > limit)
+                pq.remove(nums[lt++]);
+            maxLen = Math.max(maxLen, rt - lt);
+            // System.out.println(pq.toString());
+            pq.insert(nums[rt++]);
+        }
+        while (pq.findAbsDiff() > limit)
+            pq.remove(nums[lt++]);
+        maxLen = Math.max(maxLen, rt - lt);
+        return maxLen;
+    }
+}
