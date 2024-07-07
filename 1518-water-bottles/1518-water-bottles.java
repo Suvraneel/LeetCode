68bd094bd61ea7866b7d9c50d298b4f47096eab6
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-07-07 17:58:33 +0530

    Time: 0 ms (100%), Space: 40.3 MB (38.7%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int ans = 0, empty = 0;
+        while(numBottles>0){
+            ans += numBottles;
+            empty += numBottles;
+            numBottles = empty/numExchange;
+            empty %= numExchange;
+        }
+        return ans;
+    }
+}
