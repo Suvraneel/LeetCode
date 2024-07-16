938073a17125e95b5d16ba8f9ff0955fbef8c1d4
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-07-17 03:33:24 +0530

    Time: 37 ms (28.52%), Space: 82.5 MB (31.79%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    StringBuilder rootToNode;
+    public String getDirections(TreeNode root, int startValue, int destValue) {
+        dfs(root, startValue, new StringBuilder());
+        StringBuilder rootToStart = new StringBuilder(rootToNode);
+        dfs(root, destValue, new StringBuilder());
+        StringBuilder rootToDest = new StringBuilder(rootToNode);
+        // System.out.println(rootToStart +"\t"+ rootToDest);
+        // remove common path (ie, from root to last mutual ancestor
+        int minPathLen = Math.min(rootToStart.length(), rootToDest.length()), i;
+        for(i=0; i<minPathLen; i++)
+            if(rootToStart.charAt(i)!=rootToDest.charAt(i))
+                break;
+        // System.out.println(rootToStart +"\t"+ rootToDest);
+        return "U".repeat(rootToStart.substring(i).length()) + rootToDest.substring(i);
+    }
+    private void dfs(TreeNode root, int destValue, StringBuilder path){
+        if(root==null)
+            return;
+        if(root.val==destValue){
+            rootToNode = new StringBuilder(path);
+            return;
+        }
+        dfs(root.left, destValue, path.append("L"));
+        path.setLength(path.length()-1);
+        dfs(root.right, destValue, path.append("R"));
+        path.setLength(path.length()-1);
+    }
+}
