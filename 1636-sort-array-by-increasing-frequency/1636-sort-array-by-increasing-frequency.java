c51dfcc4843a5b7055d25b9be90a96e7f14071b3
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-07-24 03:40:02 +0530

    Time: 9 ms (32.78%), Space: 44.5 MB (40.41%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -1,20 +1,14 @@
 class Solution {
     public int[] frequencySort(int[] nums) {
-        Map<Integer, Integer> freq = new HashMap<>();
+        int[] freq = new int[201];
         for(int n: nums)
-            freq.put(n, freq.getOrDefault(n, 0)+1);
-        List<Pair<Integer, Integer>> rFreq = new ArrayList<>();
-        for(Map.Entry<Integer, Integer> e: freq.entrySet())
-            rFreq.add(new Pair<>(e.getValue(), e.getKey()));
-        rFreq = rFreq.stream().sorted(Comparator.comparingInt(Pair<Integer, Integer>::getKey).thenComparing(Pair::getValue, Comparator.reverseOrder())).collect(Collectors.toList());
-        // System.out.println(rFreq);
-        int[] ans = new int[nums.length];
-        int idx = 0;
-        for(Pair<Integer, Integer> p: rFreq){
-            int ct = p.getKey(), n = p.getValue();
-            while(ct-->0)
-                ans[idx++] = n; 
-        }
-        return ans;
+            freq[n+100]++;
+        Integer[] ans = Arrays.stream(nums).boxed().toArray(Integer[]::new);
+        Arrays.sort(ans, (a, b) -> {
+            if(freq[a+100]==freq[b+100])
+                return b - a;
+            return freq[a+100] - freq[b+100];
+        });
+        return Arrays.stream(ans).mapToInt(Integer::intValue).toArray();
     }
 }
