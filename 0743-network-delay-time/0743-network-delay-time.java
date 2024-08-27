1727ff29db57c66329f662c4975d923fb0feb842
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-08-28 01:18:04 +0530

    Time: 19 ms (39.2%), Space: 48.2 MB (65.07%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,32 +1,28 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        boolean[] vis = new boolean[n + 1];
-        vis[0] = true;
-        List<int[]>[] adj = new List[n + 1];
-        for (int i = 1; i <= n; i++)
+        List<int[]>[] adj = new List[n+1];
+        Set<Integer> vis = new HashSet<>();
+        for(int i=1; i<=n; i++)
             adj[i] = new ArrayList<>();
-        for (int[] e : times)
-            adj[e[0]].add(new int[] { e[1], e[2] });
-        return djikstra(adj, vis, n, k);
+        for(int[] edge: times)
+            adj[edge[0]].add(new int[]{edge[1], edge[2]});
+        int maxDelay = Integer.MAX_VALUE;
+        return djikstra(adj, k, vis, n);
     }
 
-    private int djikstra(List<int[]>[] adj, boolean[] vis, int n, int src) {
-        Queue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        pq.offer(new int[] { src, 0 });
-        int maxCost = -1;
-        while (!pq.isEmpty()) {
+    private int djikstra(List<int[]>[] adj, int src, Set<Integer> vis, int n){
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1],b[1]));
+        pq.offer(new int[]{src, 0});
+        while(!pq.isEmpty()){
             int[] top = pq.poll();
-            if (vis[top[0]])
+            if(vis.contains(top[0]))
                 continue;
-            maxCost = Math.max(maxCost, top[1]);
-            vis[top[0]] = true;
-            for (int[] nbr : adj[top[0]])
-                if (!vis[nbr[0]])
-                    pq.add(new int[] { nbr[0], top[1] + nbr[1] });
+            vis.add(top[0]);
+            if(vis.size()==n)
+                return top[1];
+            for(int[] nbr: adj[top[0]])
+                pq.offer(new int[]{nbr[0], top[1]+nbr[1]});
         }
-        for (boolean v : vis)
-            if (!v)
-                return -1;
-        return maxCost;
+        return -1;
     }
 }
