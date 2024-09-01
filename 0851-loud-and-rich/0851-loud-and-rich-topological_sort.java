846cf214c5ea48572c64bc61fa12f9b4b00653d4
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-09-02 03:52:06 +0530

    Time: 7 ms (85.86%), Space: 55.2 MB (38.59%) - LeetHub

---
+++ 0851-loud-and-rich/0851-loud-and-rich-topological_sort.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] loudAndRich(int[][] richer, int[] quiet) {
+        int n = quiet.length;
+        int[] memo = new int[n], indegree = new int[n];
+        Arrays.fill(memo, -1);
+        List<Integer>[] adj = new List[n];
+        for (int i = 0; i < n; i++)
+            adj[i] = new ArrayList<>();
+        for (int[] edge : richer){
+            adj[edge[1]].add(edge[0]);
+            indegree[edge[0]]++;
+        }
+        for (int i = 0; i < n; i++)
+            if(indegree[i]==0)
+                dfs(adj, quiet, memo, i);
+        return memo;
+    }
+
+    private int dfs(List<Integer>[] adj, int[] quiet, int[] memo, int node) {
+        if(memo[node]!=-1)
+            return memo[node];
+        int quietest = node;
+        for (int nbr : adj[node]) {
+            int quietestNbr = dfs(adj, quiet, memo, nbr);
+            if (quiet[quietestNbr] < quiet[quietest])
+                quietest = quietestNbr;
+        }
+        return memo[node] = quietest;
+    }
+}
