b406e33f8ad15938dba170b6345071d5d5bec81b
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-09-02 03:40:30 +0530

    Time: 6 ms (97.78%), Space: 55.4 MB (30.91%) - LeetHub

--- 0851-loud-and-rich/0851-loud-and-rich.java
+++ 0851-loud-and-rich/0851-loud-and-rich.java
@@ -1,27 +1,30 @@
 class Solution {
     public int[] loudAndRich(int[][] richer, int[] quiet) {
         int n = quiet.length;
-        int[] ans = new int[n];
+        int[] ans = new int[n], memo = new int[n];
+        Arrays.fill(memo, -1);
         List<Integer>[] adj = new List[n];
         for (int i = 0; i < n; i++)
             adj[i] = new ArrayList<>();
         for (int[] edge : richer)
             adj[edge[1]].add(edge[0]);
         for (int i = 0; i < n; i++)
-            ans[i] = dfs(adj, quiet, new boolean[n], i);
+            ans[i] = dfs(adj, quiet, memo, new boolean[n], i);
         return ans;
     }
 
-    private int dfs(List<Integer>[] adj, int[] quiet, boolean[] vis, int node) {
+    private int dfs(List<Integer>[] adj, int[] quiet, int[] memo, boolean[] vis, int node) {
+        if(memo[node]!=-1)
+            return memo[node];
         if (vis[node])
             return node;
         vis[node] = true;
         int quietest = node;
         for (int nbr : adj[node]) {
-            int quietestNbr = dfs(adj, quiet, vis, nbr);
+            int quietestNbr = dfs(adj, quiet, memo, vis, nbr);
             if (quiet[quietestNbr] < quiet[quietest])
                 quietest = quietestNbr;
         }
-        return quietest;
+        return memo[node] = quietest;
     }
 }
