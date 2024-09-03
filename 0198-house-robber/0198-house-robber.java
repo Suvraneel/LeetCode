41c40bc8581eebd1e6e095027d69f87dad516948
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-09-04 02:59:03 +0530

    Time: 0 ms (100%), Space: 40.7 MB (78.5%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,17 +1,13 @@
 class Solution {
     int n;
+
     public int rob(int[] nums) {
         this.n = nums.length;
-        int[] memo = new int[n];
-        Arrays.fill(memo, -1);
-        return solve(nums, memo, 0);
-    }
-
-    private int solve(int[] nums, int[] memo, int lt) {
-        if (lt > n - 1)
-            return 0;
-        if(memo[lt]!=-1)
-            return memo[lt];
-        return memo[lt] = Math.max(solve(nums, memo, lt + 1), nums[lt] + solve(nums, memo, lt + 2));
+        int[] memo = new int[n + 1];
+        memo[0] = 0;
+        memo[1] = nums[0];
+        for (int i = 0; i < n - 1; i++)
+            memo[i + 2] = Math.max(memo[i] + nums[i + 1], memo[i + 1]);
+        return memo[n];
     }
 }
