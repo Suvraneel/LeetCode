fc153bbd4f16d00edad9f8c4921c3cead34467f6
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-09-05 02:13:16 +0530

    Time: 0 ms (100%), Space: 40.8 MB (6.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,16 @@
 class Solution {
+    int[] memo;
+
     public int climbStairs(int n) {
-        Integer[] dp = new Integer[n+1];
-        return solve(dp, n);
+        memo = new int[n + 1];
+        return solve(n);
     }
-    private int solve(Integer[] dp, int i){
-        if(i==0)
-            return 1;
-        else if(i<0)
-            return 0;
-        if(dp[i]!=null)
-            return dp[i];
-        return dp[i] = solve(dp, i-1) + solve(dp, i-2);
+
+    private int solve(int n) {
+        if (n < 3)
+            return n;
+        if (memo[n] != 0)
+            return memo[n];
+        return memo[n] = solve(n - 1) + solve(n - 2);
     }
 }
