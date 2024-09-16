4f5db4d1b141b6cb22e1e53dfda295287da4f0e3
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-09-16 18:23:19 +0530

    Time: 5 ms (67.04%), Space: 45.2 MB (71.57%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.java
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -3,12 +3,11 @@ class Solution {
         int n = timePoints.size(), minDiff = Integer.MAX_VALUE;
         int[] timeInMins = new int[n];
         for (int i = 0; i < n; i++) {
-            String[] t = timePoints.get(i).split(":");
-            timeInMins[i] = Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
+            timeInMins[i] = Integer.parseInt(timePoints.get(i).substring(0,2)) * 60 + Integer.parseInt(timePoints.get(i).substring(3));
         }
         Arrays.sort(timeInMins);
         for (int i = 1; i < n; i++)
             minDiff = Math.min(minDiff, timeInMins[i] - timeInMins[i - 1]);
-        return minDiff = Math.min(minDiff, 24 * 60 - timeInMins[n-1] + timeInMins[0]);
+        return minDiff = Math.min(minDiff, 24 * 60 - timeInMins[n - 1] + timeInMins[0]);
     }
 }
