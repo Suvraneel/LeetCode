2611023ee465b535d9abe1a441ed50fb80cd9e68
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-09-19 02:23:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        String[] s = Arrays.stream(nums).mapToObj(k -> String.valueOf((Integer) k)).toArray(String[]::new);
+        Arrays.sort(s, (a, b) -> (b + a).compareTo(a + b));
+        // System.out.println(Arrays.toString(s));
+        if(s[0].equals("0"))
+            return 0;
+        StringBuilder sb = new StringBuilder();
+        for (String w : s)
+            sb.append(w);
+        return sb.toString();
+    }
+}
