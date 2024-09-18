e038bf394fc497b45b018172cd0e4a43a5295d04
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-09-19 02:25:01 +0530

    Time: 8 ms (20.98%), Space: 43.1 MB (65.43%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -3,7 +3,7 @@ class Solution {
         String[] s = Arrays.stream(nums).mapToObj(k -> String.valueOf((Integer) k)).toArray(String[]::new);
         Arrays.sort(s, (a, b) -> (b + a).compareTo(a + b));
         // System.out.println(Arrays.toString(s));
-        if(s[0].equals("0"))
+        if (s[0].equals("0"))   // implies all zero elements
             return "0";
         StringBuilder sb = new StringBuilder();
         for (String w : s)
