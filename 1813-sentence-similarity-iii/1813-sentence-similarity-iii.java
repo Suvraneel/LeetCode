60d34bf95fb2e3cbf39501cfb88239e588041ff9
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-10-06 22:34:51 +0530

    Time: 9 ms (8.73%), Space: 41.8 MB (39.29%) - LeetHub

---
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        if (sentence2.length() > sentence1.length()) {
+            String temp = sentence2;
+            sentence2 = sentence1;
+            sentence1 = temp;
+        }
+        String[] words1 = sentence1.split(" "), words2 = sentence2.split(" ");
+        int n1 = words1.length, n2 = words2.length, lt = 0, rt = 0;
+        while (lt < n2 && words1[lt].equals(words2[lt]))
+            lt++;
+        while (rt < n2 && words1[n1 - 1 - rt].equals(words2[n2 - 1 - rt]))
+            rt++;
+        System.out.println(lt + "\t" + rt + "\t" + n1 + "\t" + n2);
+        if (lt + rt < n2)
+            return false;
+        return true;
+    }
+}
