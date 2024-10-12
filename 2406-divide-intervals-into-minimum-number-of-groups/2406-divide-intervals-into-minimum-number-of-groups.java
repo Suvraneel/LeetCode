aed5f011d6a66e9fc34cc609295abe936a648909
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-10-13 01:17:10 +0530

    Time: 26 ms (95.08%), Space: 94.8 MB (15.8%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
@@ -1,13 +1,18 @@
 class Solution {
     public int minGroups(int[][] intervals) {
-        Map<Integer, Integer> cal = new TreeMap<>(); // calendar
+        int minm = Integer.MAX_VALUE, maxm = Integer.MIN_VALUE;
         for (int[] interval : intervals) {
-            cal.put(interval[0], cal.getOrDefault((interval[0]), 0) + 1);
-            cal.put(interval[1] + 1, cal.getOrDefault(interval[1] + 1, 0) - 1);
+            minm = Math.min(minm, interval[0]);
+            maxm = Math.max(maxm, interval[1]);
+        }
+        int[] cal = new int[maxm - minm + 2]; // calendar
+        for (int[] interval : intervals) {
+            cal[interval[0] - minm]++;
+            cal[interval[1] + 1 - minm]--;
         }
         int minDisjointGrp = 0, concurrentInterval = 0;
-        for(Integer e: cal.values())
-            minDisjointGrp = Math.max(minDisjointGrp, concurrentInterval += e);
+        for (int i : cal)
+            minDisjointGrp = Math.max(minDisjointGrp, concurrentInterval += i);
         // System.out.println(cal);
         return minDisjointGrp;
     }
