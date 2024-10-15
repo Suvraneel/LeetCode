c09475564a5581b6f8b38a6747e471d1eb7f6a03
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-10-16 03:54:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -5,12 +5,17 @@ class Solution {
         this.nums = nums;
         this.target = target;
         this.n = nums.length;
-        return solve(0, 0);
+        int[][] dp = new int[n][target];
+        for(int i=0; i<target; i++)
+            Arrays.fill(dp[i], -1);
+        return solve(0, 0, dp);
     }
-    private int solve(int idx, int total){
+    private int solve(int idx, int total, int[][] dp){
         if(idx == n)
             return total == target ? 1 : 0;
-        return 
-            solve(idx + 1, total + nums[idx]) + solve(idx + 1, total - nums[idx]);
+        if(dp[idx][total]!=-1)
+            return dp[idx][total];
+        return dp[idx][total] =
+            solve(idx + 1, total + nums[idx], dp) + solve(idx + 1, total - nums[idx], dp);
     }
 }
