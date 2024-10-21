9fd7af7db44f55d151aaad595be84fe7afba014d
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-10-21 17:24:02 +0530

    Time: 33 ms (73.86%), Space: 45.5 MB (6.54%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.java
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.java
@@ -1,19 +1,22 @@
 class Solution {
+    int maxAns = 0;
 
     public int maxUniqueSplit(String s) {
         Set<String> seen = new HashSet<>();
-        return solve(s, 0, s.length(), seen);
+        return solve(s, 0, s.length(), 0, seen);
     }
 
-    private int solve(String s, int idx, int n, Set<String> seen) {
+    private int solve(String s, int idx, int n, int ct, Set<String> seen) {
         if (idx == n)
+            return ct;
+        if (maxAns > ct + n - idx)
             return 0;
         int maxUniqueSubs = 0;
         for (int i = idx + 1; i <= n; i++) {
             String sub = s.substring(idx, i);
             if (!seen.contains(sub)) {
                 seen.add(sub);
-                maxUniqueSubs = Math.max(1 + solve(s, i, n, seen), maxUniqueSubs);
+                maxUniqueSubs = Math.max(solve(s, i, n, ct + 1, seen), maxUniqueSubs);
                 seen.remove(sub);
             }
         }
