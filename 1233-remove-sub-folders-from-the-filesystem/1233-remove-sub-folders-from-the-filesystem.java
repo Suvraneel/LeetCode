cda8acd49387100b25afdd7fdf3754d4c2d7e626
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-10-25 22:20:59 +0530

    Time: 81 ms (26.94%), Space: 56.8 MB (34.49%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
@@ -11,25 +11,38 @@ class Solution {
             root = new TrieNode();
         }
 
-        boolean insert(String folder) {
+        void insert(String folder) {
             String[] path = folder.split("/");
             TrieNode curr = root;
             for (String dir : path) {
                 curr.children.putIfAbsent(dir, new TrieNode());
                 curr = curr.children.get(dir);
-                if (curr.isVisited)
-                    return false;
+                if(curr.isVisited)  // skip traversing a nested path of already seen dir
+                    return;
             }
-            return curr.isVisited = true;
+            curr.children.clear();  // clear subfolders encountered previously
+            curr.isVisited = true;
+        }
+
+        boolean contains(String folder) {
+            String[] path = folder.split("/");
+            TrieNode curr = root;
+            for (String dir : path) {
+                if(!curr.children.containsKey(dir))
+                    return false;
+                curr = curr.children.get(dir);
+            }
+            return curr.isVisited;
         }
     }
 
     public List<String> removeSubfolders(String[] folder) {
-        Arrays.sort(folder);
-        List<String> prunedFolderList = new ArrayList<>();
         Trie trie = new Trie();
         for (String f : folder)
-            if (trie.insert(f))
+            trie.insert(f);
+        List<String> prunedFolderList = new ArrayList<>();
+        for(String f: folder)
+            if(trie.contains(f))
                 prunedFolderList.add(f);
         return prunedFolderList;
     }
