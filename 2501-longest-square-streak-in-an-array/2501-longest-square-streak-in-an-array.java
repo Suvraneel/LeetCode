eb3b30d11fc1ca359133e3ffa8ba41a7159746f0
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-10-28 16:29:39 +0530

    Time: 60 ms (21.88%), Space: 56.3 MB (98.61%) - LeetHub

--- 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.java
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.java
@@ -3,11 +3,12 @@ class Solution {
         Arrays.sort(nums);
         int ans = 0;
         Map<Integer, Integer> m = new HashMap<>();
-        for(int i: nums){
-            int root = (int)Math.sqrt(i);
-            if(root*root==i && m.containsKey(root))
-                m.put(i, m.get(root)+1);
-            else m.put(i, 1);
+        for (int i : nums) {
+            int root = (int) Math.sqrt(i);
+            if (root * root == i && m.containsKey(root))
+                m.put(i, m.get(root) + 1);
+            else
+                m.put(i, 1);
             ans = Math.max(ans, m.get(i));
         }
         return ans > 1 ? ans : -1;
