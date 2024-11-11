ff91545080ee0676f92ae16c0dcb13b0ebce33bd
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-11-11 23:07:35 +0530

    Time: 23 ms (7.02%), Space: 44.7 MB (41.72%) - LeetHub

--- 2601-prime-subtraction-operation/2601-prime-subtraction-operation.java
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.java
@@ -2,22 +2,20 @@ class Solution {
     public boolean primeSubOperation(int[] nums) {
         int n = nums.length;
         boolean[] primes = sieveOfEratosthenes(nums);
-        // System.out.println(Arrays.toString(primes));
+        System.out.println(Arrays.toString(primes));
         int currVal = 1;
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length;) {
             int diff = nums[i] - currVal;
-            for (int j = diff - 1; j >= 0; j--)
-                if (primes[j]) {
-                    nums[i] -= j;
-                    break;
-                }
-            currVal = nums[i];
+            if (diff < 0)
+                return false;
+            if (primes[diff] || diff == 0) {
+                i++;
+                currVal++;
+            } else
+                currVal++;
             // System.out.println(Arrays.toString(nums));
         }
-        for (int i = 1; i < nums.length - 1; i++)
-            if (nums[i] <= nums[i - 1] || nums[i] >= nums[i + 1])
-                return false;
-        return n > 2 ? true : nums[1] > nums[0];
+        return true;
     }
 
     private boolean[] sieveOfEratosthenes(int[] nums) {
