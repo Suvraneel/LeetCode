e46c5b37eae0290a08acf4044d596f00e4a663f8
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-11-22 02:29:16 +0530

    Time: 32 ms (18.97%), Space: 67.6 MB (61.45%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.java
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.java
@@ -1,26 +1,30 @@
 class Solution {
-    private long getId(int m, int n, int i, int j){
-        return n*i + j;
+    private long getId(int m, int n, int i, int j) {
+        return n * i + j;
     }
-    private static int[][] dir = {{0,1}, {1,0}, {0, -1}, {-1, 0}};
+
+    private static int[][] dir = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        Set<Long> vis = new HashSet<>();
+        int count = 0;
         int[][] grid = new int[m][n];
-        for(int[] w: walls){
-            grid[w[0]][w[1]] = 1;
-            vis.add(getId(m, n, w[0], w[1]));
-        }
-        for(int[] g: guards){
-            grid[g[0]][g[1]] = 1;
-            vis.add(getId(m, n, g[0], g[1]));
-        }
-        for(int[] g: guards){
-            for(int[] d: dir){
-                for(int i=g[0]+d[0], j=g[1]+d[1]; i>=0 && i<m && j>=0 && j<n && grid[i][j] != 1; i+=d[0], j+=d[1])
-                    vis.add(getId(m, n, i, j));
+        for (int[] w : walls)
+            grid[w[0]][w[1]] = 1; // blocked
+        for (int[] g : guards)
+            grid[g[0]][g[1]] = 1; // blocked
+        for (int[] g : guards) {
+            grid[g[0]][g[1]] = 2; // unblock temp.
+            for (int[] d : dir) {
+                for (int i = g[0], j = g[1]; i >= 0 && i < m && j >= 0 && j < n
+                        && grid[i][j] != 1; i += d[0], j += d[1]) {
+                    if (grid[i][j] == 0) // new
+                        count++;
+                    grid[i][j] = 2; // visited
+                }
             }
+            grid[g[0]][g[1]] = 1; // block back
         }
-        // System.out.println(vis);
-        return m*n - vis.size();
+        // System.out.println(count);
+        return m * n - count - guards.length - walls.length;
     }
 }
