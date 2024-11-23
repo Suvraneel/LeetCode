2c834141f025b295c3286fdc4b0bda0a29abee91
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-11-24 03:28:12 +0530

    Time: 27 ms (19.35%), Space: 79.6 MB (36.41%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.java
+++ 1861-rotating-the-box/1861-rotating-the-box.java
@@ -6,7 +6,7 @@ class Solution {
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
                 box90[i][j] = box[m - 1 - j][i];
-        // turn on gravity
+        // turn on gravity... free fall along y axis
         for (int i = n - 1; i >= 0; i--) {
             for (int j = 0; j < m; j++)
                 if (box90[i][j] == '#') {
