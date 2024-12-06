c64600bed47e30866e5417eb9be26ec0ab04474f
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-12-06 16:41:09 +0530

    Time: 50 ms (33.15%), Space: 46 MB (22.73%) - LeetHub

---
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i-sort.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxCount(int[] banned, int n, int maxSum) {
+        Arrays.sort(banned);
+        int b = 0, blen = banned.length, sum = 0, ct = 0;
+        for (int i = 1; i <= n && sum <= maxSum; i++) {
+            while (b < blen && banned[b] < i)
+                b++;
+            if (b < blen && banned[b] == i)
+                continue;
+            sum += i;
+            ct++;
+        }
+        System.out.println(sum);
+        return sum > maxSum ? ct - 1 : ct;
+    }
+}
