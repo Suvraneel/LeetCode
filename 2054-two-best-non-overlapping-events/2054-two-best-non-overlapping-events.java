b5430164c9f81aaca217f809a6eeecd7e8948bd2
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-12-08 18:54:40 +0530

    Time: 62 ms (38.51%), Space: 116.3 MB (38.29%) - LeetHub

---
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxTwoEvents(int[][] events) {
+        List<int[]> lineSweep = new ArrayList<>();
+        for (int[] e : events) {
+            lineSweep.add(new int[] { e[0], 1, e[2] });
+            lineSweep.add(new int[] { e[1] + 1, -1, e[2] });
+        }
+        lineSweep.sort((a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        int maxVal = 0, ans = 0;
+        for (int[] t : lineSweep) {
+            if (t[1] == 1) {
+                ans = Math.max(ans, t[2] + maxVal); // sum of curr & max val of prev ended non-overlapped
+            } else
+                maxVal = Math.max(maxVal, t[2]); // track max non-overlapped value in line yet
+        }
+        return ans;
+    }
+}
