2ca9ebb75b102013f8dfcabe149316e9f81e8c39
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-12-13 19:26:19 +0530

    Time: 98 ms (98.04%), Space: 57.7 MB (70.83%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.java
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.java
@@ -1,14 +1,14 @@
 class Solution {
     public long findScore(int[] nums) {
-        LinkedList<int[]> ll = new LinkedList<>();
+        List<int[]> l = new ArrayList<>();
         int n = nums.length;
         long score = 0;
         for (int i = 0; i < n; i++)
-            ll.add(new int[] { nums[i], i });
-        // ll.sort((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
-        ll.sort((a, b) -> a[0] - b[0]); // Stable sort, so not reqd. ^
-        while (!ll.isEmpty()) {
-            int[] top = ll.poll();
+            l.add(new int[] { nums[i], i });
+        // l.sort((a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+        l.sort((a, b) -> a[0] - b[0]); // Stable sort, so not reqd. ^
+        for (int i = 0; i < n; i++) {
+            int[] top = l.get(i);
             if (nums[top[1]] == 0)
                 continue;
             score += top[0];
