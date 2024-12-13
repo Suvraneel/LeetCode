fe3d9aa3b2d05250202bad7831b4e111194e1ced
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-12-13 18:57:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long findScore(int[] nums) {
+        Queue<int[]> pq = new PriorityQueue<>(
+                (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
+        int score = 0, n = nums.length;
+        for (int i = 0; i < n; i++)
+            pq.offer(new int[] { nums[i], i });
+        while (!pq.isEmpty()) {
+            int[] top = pq.poll();
+            if (nums[top[1]] == 0)
+                continue;
+            score += top[0];
+            nums[top[1]] = 0;
+            if (top[1] > 0)
+                nums[top[1] - 1] = 0;
+            if (top[1] < n - 1)
+                nums[top[1] + 1] = 0;
+        }
+        return score;
+    }
+}
