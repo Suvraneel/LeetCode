e7e13ef4dd31ffea9e2568de287713f7dff35143
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-12-26 17:59:07 +0530

    Time: 2455 ms (5.06%), Space: 41.3 MB (80.82%) - LeetHub

--- 0494-target-sum/0494-target-sum-brute-force.java
+++ 0494-target-sum/0494-target-sum-brute-force.java
@@ -1,16 +1,20 @@
 class Solution {
-    int[] nums;
-    int target, n;
     public int findTargetSumWays(int[] nums, int target) {
-        this.nums = nums;
-        this.target = target;
-        this.n = nums.length;
-        return solve(0, 0);
+        int n = nums.length;
+        int[] signs = new int[n];
+        Arrays.fill(signs, -1);
+        return solve(signs, nums, target, n, 0);
     }
-    private int solve(int idx, int total){
-        if(idx == n)
-            return total == target ? 1 : 0;
-        return 
-            solve(idx + 1, total + nums[idx]) + solve(idx + 1, total - nums[idx]);
+
+    private int solve(int[] signs, int[] nums, int target, int n, int idx) {
+        int ct = 0;
+        for (int i = idx; i < n; i++) {
+            signs[i] = 1;
+            ct += solve(signs, nums, target, n, i + 1);
+            signs[i] = -1;
+        }
+        for (int i = 0; i < n; i++)
+            target -= nums[i] * signs[i];
+        return target == 0 ? ++ct : ct;
     }
 }
