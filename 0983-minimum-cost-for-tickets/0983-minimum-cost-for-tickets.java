d3dc4e0ac7436db7cfba755efe5a2fec626ca98c
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-01-01 02:33:27 +0530

    Time: 0 ms (100%), Space: 42 MB (25.68%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,29 +1,22 @@
 class Solution {
-    int[] days, costs, memo;
-    int n;
+    int[] passes = { 1, 7, 30 };
 
     public int mincostTickets(int[] days, int[] costs) {
-        this.days = days;
-        this.costs = costs;
-        this.n = days.length;
-        memo = new int[n + 1];
-        Arrays.fill(memo, -1);
-        return solve(0);
+        int[] memo = new int[days.length + 1];
+        return solve(days, costs, 0, memo);
     }
 
-    private int solve(int idx) {
-        if (idx >= n)
+    private int solve(int[] days, int[] costs, int dayIdx, int[] memo) {
+        if (dayIdx == days.length)
             return 0;
-        if (memo[idx] != -1)
-            return memo[idx];
-        int i = idx, date = days[idx++];
-        int pass1 = costs[0] + solve(idx);
-        while (idx < n && date + 7 > days[idx])
-            idx++;
-        int pass2 = costs[1] + solve(idx);
-        while (idx < n && date + 30 > days[idx])
-            idx++;
-        int pass3 = costs[2] + solve(idx);
-        return memo[i] = Math.min(pass1, Math.min(pass2, pass3));
+        if (memo[dayIdx] != 0)
+            return memo[dayIdx];
+        int $ = Integer.MAX_VALUE, idx = dayIdx + 1;
+        for (int i = 0; i < 3; i++) {
+            while (idx < days.length && days[idx] < days[dayIdx] + passes[i])
+                idx++;
+            $ = Math.min($, costs[i] + solve(days, costs, idx, memo));
+        }
+        return memo[dayIdx] = $;
     }
 }
