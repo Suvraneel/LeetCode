0abf400c61608a6c8a87deb1df8b470114b16038
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-01-05 23:32:34 +0530

    Time: 7 ms (74.53%), Space: 73.2 MB (93.4%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String shiftingLetters(String s, int[][] shifts) {
+        int n = s.length(), prefixSum = 0;
+        int[] conversion = new int[n+1];
+        for(int[] sh: shifts){
+            if(sh[2]==1){
+                conversion[sh[0]]++;
+                conversion[sh[1]+1]--;
+            } else {
+                conversion[sh[0]]--;
+                conversion[sh[1]+1]++;
+            }
+        }
+        char[] cs = s.toCharArray();
+        for(int i=0; i<n; i++){
+            prefixSum = ((prefixSum + conversion[i]) % 26 + 26) % 26;
+            cs[i] = (char)((cs[i] - 'a' + prefixSum) % 26 + 'a');
+        }
+        return new String(cs);
+    }
+}
