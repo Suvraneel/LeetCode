0e28d1bcec995c5f02b6fc3630a869e9c7d4760b
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-01-09 20:20:57 +0530

    Update 2185-counting-words-with-a-given-prefix-1-liner-stream-lambda.java

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix-1-liner-stream-lambda.java
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix-1-liner-stream-lambda.java
@@ -1,9 +1,5 @@
 class Solution {
     public int prefixCount(String[] words, String pref) {
-        int count = 0;
-        for(String s: words)
-            if(s.startsWith(pref))
-                count++;
-        return count;
+        return (int)Arrays.stream(words).filter(w->w.startsWith(pref)).count();
     }
 }
