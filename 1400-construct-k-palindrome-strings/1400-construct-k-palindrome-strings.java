ae87e05c902e0524329dde5958d999ae81c5c196
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-01-11 22:59:18 +0530

    Time: 3 ms (100%), Space: 45.7 MB (11.51%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.java
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.java
@@ -4,13 +4,9 @@ class Solution {
             return false;
         if (k == s.length())
             return true;
-        int[] freq = new int[26];
-        for (char c : s.toCharArray())
-            freq[c - 'a']++;
         int odd = 0;
-        for (int i = 0; i < 26; i++)
-            if (freq[i] % 2 == 1)
-                odd++;
-        return k >= odd;
+        for (char c : s.toCharArray())
+            odd ^= 1 << c - 'a';
+        return k >= Integer.bitCount(odd);
     }
 }
