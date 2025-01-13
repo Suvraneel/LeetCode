0086b9e0586eefc6fb514ac16db424c17f926522
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-01-13 17:01:27 +0530

    Time: 15 ms (54.3%), Space: 46.2 MB (66.89%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.java
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.java
@@ -1,9 +1,9 @@
 class Solution {
     public int minimumLength(String s) {
         int[] freq = new int[26];
-        int minLen = 0;
         for (byte c : s.getBytes())
             freq[c - 'a']++;
+        int minLen = 0;
         for (int i: freq)
             if(i>0)
                 minLen += 2 - (i & 1);
