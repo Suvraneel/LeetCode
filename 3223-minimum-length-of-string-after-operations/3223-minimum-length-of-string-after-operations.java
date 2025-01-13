98ed50b6ea35c054f0ff156d9da7fd79a7910011
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-01-13 17:03:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.java
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.java
@@ -3,8 +3,8 @@ class Solution {
         int[] freq = new int[26];
         for (byte c : s.getBytes())
             freq[c - 'a']++;
-        int minLen = 0;
         for (int i: freq)
+        int minLen = 0;
             if(i>0)
                 minLen += 2 - (i & 1);
         return minLen;
