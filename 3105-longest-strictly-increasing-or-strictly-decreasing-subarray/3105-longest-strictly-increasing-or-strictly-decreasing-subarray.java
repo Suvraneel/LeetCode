a5bbc28731d1da3a615c4e953036e522226ec0c8
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-02-03 21:26:36 +0530

    Time: 1 ms (93.94%), Space: 42.5 MB (82.83%) - LeetHub

--- 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         if (n < 2)
             return n;
-        int longestInc = 0, longestDec = 0, incStreak = 1, decStreak = 1;
+        int maxMonotonicLen = 0, incStreak = 1, decStreak = 1;
         for (int i = 1; i < n; i++) {
             if (nums[i] > nums[i - 1]) {
                 incStreak++;
@@ -13,9 +13,8 @@ class Solution {
                 incStreak = 1;
             } else
                 incStreak = decStreak = 1;
-            longestInc = Math.max(longestInc, incStreak);
-            longestDec = Math.max(longestDec, decStreak);
+            maxMonotonicLen = Math.max(maxMonotonicLen, Math.max(incStreak, decStreak));
         }
-        return Math.max(longestInc, longestDec);
+        return maxMonotonicLen;
     }
 }
