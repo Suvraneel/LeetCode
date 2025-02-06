f78869ca685f60675e511aa9ca32acd3d56fbdb9
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-02-06 18:32:36 +0530

    Time: 212 ms (62.82%), Space: 68.9 MB (59.62%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.java
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.java
@@ -2,15 +2,15 @@ class Solution {
     public int tupleSameProduct(int[] nums) {
         int n = nums.length, count = 0;
         Map<Integer, Integer> pdtMap = new HashMap<>();
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                int pdt = nums[i]*nums[j];
-                pdtMap.put(pdt, pdtMap.getOrDefault(pdt, 0)+1);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int pdt = nums[i] * nums[j];
+                pdtMap.put(pdt, pdtMap.getOrDefault(pdt, 0) + 1);
             }
         }
-        for(Integer freq: pdtMap.values())
-            if(freq>1)
-                count += freq * (freq - 1) * 2 * 2;  // nP2 * 2P2 * 2P2
+        for (Integer freq : pdtMap.values())
+            if (freq > 1)
+                count += freq * (freq - 1) * 4; // nP2 * 2P2 * 2P2
         return count;
     }
 }
