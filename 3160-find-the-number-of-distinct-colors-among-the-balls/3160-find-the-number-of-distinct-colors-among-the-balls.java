a4158e7596c190162a747dd663d8c29d34cd9c45
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-02-08 04:46:49 +0530

    Time: 37 ms (96.57%), Space: 97.5 MB (39.48%) - LeetHub

--- 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.java
+++ 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] queryResults(int limit, int[][] queries) {
-        HashMap<Integer, Integer> colorFreqMap = new HashMap<>();
-        HashMap<Integer, Integer> ballColorMap = new HashMap<>();
+        HashMap<Integer, Integer> colorsFreqMap = new HashMap<>();
+        HashMap<Integer, Integer> ballsColorMap = new HashMap<>();
         int[] result = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
             int x = queries[i][0], y = queries[i][1];
-            if (ballColorMap.containsKey(x)) {
-                int oldColor = ballColorMap.get(x);
-                colorFreqMap.put(oldColor, colorFreqMap.get(oldColor) - 1);
-                if (colorFreqMap.get(oldColor) == 0)
-                    colorFreqMap.remove(oldColor);
+            if (ballsColorMap.containsKey(x)) {
+                int oldColor = ballsColorMap.get(x);
+                colorsFreqMap.put(oldColor, colorsFreqMap.get(oldColor) - 1);
+                if (colorsFreqMap.get(oldColor) == 0)
+                    colorsFreqMap.remove(oldColor);
             }
-            colorFreqMap.put(y, colorFreqMap.getOrDefault(y, 0) + 1);
-            ballColorMap.put(x, y);
-            result[i] = colorFreqMap.size();
+            colorsFreqMap.put(y, colorsFreqMap.getOrDefault(y, 0) + 1);
+            ballsColorMap.put(x, y);
+            result[i] = colorsFreqMap.size();
         }
         return result;
     }
