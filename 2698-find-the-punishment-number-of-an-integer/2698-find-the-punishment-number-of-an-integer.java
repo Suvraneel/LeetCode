03c9384c58a20aa657a1229f4d4031ca3538c5c2
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-02-15 17:34:40 +0530

    Time: 67 ms (65.52%), Space: 40.6 MB (90.91%) - LeetHub

---
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int punishmentNumber(int n) {
+        int punishNum = 0;
+        for (int i = 1; i <= n; i++)
+            if (validTerm(i, i*i)) {
+                // System.out.println(i+"✅");
+                punishNum += i * i;
+            }
+        return punishNum;
+    }
+
+    private boolean validTerm(int match, int ltPart) {
+        int rtPart = 0;
+        // System.out.println(match + "===>");
+
+        for (int d = 0; ltPart > 0; d++) {
+            rtPart = (int) Math.pow(10, d) * (ltPart % 10) + rtPart;
+            ltPart /= 10;
+            // System.out.println(ltPart+"+"+rtPart);
+            if (ltPart + rtPart == match || validTerm(match-rtPart, ltPart))
+                return true;
+        }
+        return false;
+    }
+}
