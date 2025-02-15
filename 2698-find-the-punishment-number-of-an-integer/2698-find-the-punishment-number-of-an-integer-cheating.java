41331a0054c722e1d82377673f83977730b82fc6
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-02-15 17:48:16 +0530

    Time: 0 ms (100%), Space: 40.8 MB (85.27%) - LeetHub

---
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer-cheating.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int punishmentNumber(int n) {
+        int[] validTerms = {1, 9, 10, 36, 45, 55, 82, 91, 99, 100, 235, 297, 369, 370, 379, 414, 657,
+                675, 703, 756, 792, 909, 918, 945, 964, 990, 991, 999, 1000};
+        int punishNum = 0;
+        for (int i : validTerms) {
+            if (i > n)
+                break;
+            else
+                punishNum += i * i;
+        }
+        return punishNum;
+    }
+}
