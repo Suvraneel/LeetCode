1997cc023669f7c1324bde726189b11b46b348b1
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-02-28 04:17:25 +0530

    Time: 197 ms (33.91%), Space: 45 MB (88.7%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.java
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.java
@@ -2,10 +2,10 @@ class Solution {
     public int lenLongestFibSubseq(int[] arr) {
         int n = arr.length, maxStreak = 0;
         Set<Integer> set = Arrays.stream(arr).boxed().collect(Collectors.toSet());
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
                 int fib1 = arr[i], fib2 = arr[j], fib3 = fib1 + fib2, streak = 2;
-                while(set.contains(fib3)){
+                while (set.contains(fib3)) {
                     streak++;
                     fib1 = fib2;
                     fib2 = fib3;
