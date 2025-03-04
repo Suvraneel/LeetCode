272f6affbb6e02f45f954520d3d02283feb906e7
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-03-04 16:32:52 +0530

    Time: 0 ms (100%), Space: 40.9 MB (14.24%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.java
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,16 +1,10 @@
 class Solution {
     public boolean checkPowersOfThree(int n) {
-        int[] exp = new int[16];
-        exp[0] = 1;
-        int i = 1;
-        for (; i < 16 && exp[i - 1] < n; i++)
-            exp[i] = exp[i - 1] * 3;
-        for (int rt = --i, sum = n; rt >= 0 && sum > 0; rt--) {
-            if (exp[rt] <= sum)
-                sum -= exp[rt];
-            if (sum == 0)
-                return true;
+        while (n > 0) {
+            if (n % 3 == 2) // extract current LSB
+                return false;
+            n /= 3; // right shift ternary bit
         }
-        return false;
+        return true;
     }
 }
