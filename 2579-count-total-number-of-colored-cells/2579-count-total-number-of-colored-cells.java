abe5483d721af8dc16eeed9d7c013bc6a28cf734
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-03-05 21:28:25 +0530

    Time: 3 ms (42.21%), Space: 40.2 MB (85.66%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.java
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.java
@@ -1,6 +1,6 @@
 class Solution {
     public long coloredCells(int n) {
-        long add = 0, total = 1;
+        long total = 1;
         for(int min = 1; min < n; min++)
             total += 4 * min;
         return total;
