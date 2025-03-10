5976b446f46e3c53c5d33243080f3e9f50f29c0a
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-03-11 00:05:29 +0530

    Time: 214 ms (54.7%), Space: 49.7 MB (18.8%) - LeetHub

--- 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
+++ 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
@@ -1,30 +1,85 @@
 class Solution {
+
     public long countOfSubstrings(String word, int k) {
-        int n = word.length(), lt = 0, rt = 0, con = 0, a = 0, e = 0, i = 0, o = 0, u = 0;
-        long ans = 0;
-        for (; rt < n; rt++) {
-            switch (word.charAt(rt)) {
-                case 'a' -> a++;
-                case 'e' -> e++;
-                case 'i' -> i++;
-                case 'o' -> o++;
-                case 'u' -> u++;
-                default -> con++;
+        long numValidSubstrings = 0;
+        int start = 0;
+        int end = 0;
+        // keep track of counts of vowels and consonants
+        HashMap<Character, Integer> vowelCount = new HashMap<>();
+        int consonantCount = 0;
+
+        // compute index of next consonant for all indices
+        int[] nextConsonant = new int[word.length()];
+        int nextConsonantIndex = word.length();
+        for (int i = word.length() - 1; i >= 0; i--) {
+            nextConsonant[i] = nextConsonantIndex;
+            if (!isVowel(word.charAt(i))) {
+                nextConsonantIndex = i;
             }
-            // System.out.println("" + con + a + e + i + o + u);
-            while (con > k) {
-                switch (word.charAt(lt++)) {
-                    case 'a' -> a--;
-                    case 'e' -> e--;
-                    case 'i' -> i--;
-                    case 'o' -> o--;
-                    case 'u' -> u--;
-                    default -> con--;
-                }
-            }
-            if (con == k && a > 0 && e > 0 && i > 0 && o > 0 && u > 0)
-                ans++;
         }
-        return ans;
+
+        // start sliding window
+        while (end < word.length()) {
+            // insert new letter
+            char newLetter = word.charAt(end);
+
+            // update counts
+            if (isVowel(newLetter)) {
+                vowelCount.put(
+                    newLetter,
+                    vowelCount.getOrDefault(newLetter, 0) + 1
+                );
+            } else {
+                consonantCount++;
+            }
+
+            // shrink window if too many consonants in our window
+            while (consonantCount > k) {
+                char startLetter = word.charAt(start);
+                if (isVowel(startLetter)) {
+                    vowelCount.put(
+                        startLetter,
+                        vowelCount.get(startLetter) - 1
+                    );
+                    if (vowelCount.get(startLetter) == 0) {
+                        vowelCount.remove(startLetter);
+                    }
+                } else {
+                    consonantCount--;
+                }
+                start++;
+            }
+
+            // while we have a valid window, try to shrink it
+            while (
+                start < word.length() &&
+                vowelCount.keySet().size() == 5 &&
+                consonantCount == k
+            ) {
+                // count the current valid substring, as well as valid substrings produced by appending more vowels
+                numValidSubstrings += nextConsonant[end] - end;
+                char startLetter = word.charAt(start);
+                if (isVowel(startLetter)) {
+                    vowelCount.put(
+                        startLetter,
+                        vowelCount.get(startLetter) - 1
+                    );
+                    if (vowelCount.get(startLetter) == 0) {
+                        vowelCount.remove(startLetter);
+                    }
+                } else {
+                    consonantCount--;
+                }
+
+                start++;
+            }
+            end++;
+        }
+
+        return numValidSubstrings;
+    }
+
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 }
