5c283df602569e53ddc1410e75e316a6d4b77012
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-03-14 18:14:10 +0530

    Time: 25 ms (73.66%), Space: 63.3 MB (33.2%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -1,7 +1,8 @@
 class Solution {
     public int maximumCandies(int[] candies, long k) {
-        Arrays.sort(candies);
-        int n = candies.length, lt = 1, rt = candies[n - 1], ans = 0;
+        int n = candies.length, lt = 1, rt = -1, ans = 0;
+        for(int c: candies)
+            rt = Math.max(rt, c);
         while (lt <= rt) {
             int mid = (lt + rt) / 2;
             if (isValidDistrib(candies, n, k, mid)) {
@@ -15,14 +16,8 @@ class Solution {
 
     private boolean isValidDistrib(int[] candies, int n, long k, int c) {
         long fed = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            int extra = candies[i] / c;
-            fed += extra;
-            if (fed >= k)
-                return true;
-            if (extra == 0)
-                return false;
-        }
-        return false;
+        for (int i = n - 1; i >= 0; i--)
+            fed += candies[i] / c;
+        return fed >= k;
     }
 }
