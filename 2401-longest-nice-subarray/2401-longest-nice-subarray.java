d7a5af0c2e78b6779400c25908ff81858ab57f55
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-03-18 18:08:43 +0530

    Time: 3 ms (98.64%), Space: 56 MB (33.79%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.java
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.java
@@ -1,11 +1,10 @@
 class Solution {
     public int longestNiceSubarray(int[] nums) {
-        int n = nums.length, maxLen = 1, lt = 0, rt = 0;
+        int n = nums.length, maxLen = 1, lt = 0, rt = 0, resBit = 0;
         for (; rt < n; rt++) {
-            for (int i = lt; i < rt; i++) {
-                if ((nums[i] & nums[rt]) != 0)
-                    lt = i + 1;
-            }
+            while ((resBit & nums[rt]) != 0)
+                resBit ^= nums[lt++]; // shrink window
+            resBit |= nums[rt]; // expand window
             maxLen = Math.max(maxLen, rt - lt + 1);
         }
         return maxLen;
