d0d5a4bc1d3a58f08aa9b9eabbd6ee589018b55f
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-03-24 21:41:12 +0530

    Time: 247 ms (7.74%), Space: 103.8 MB (9.09%) - LeetHub

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.java
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.java
@@ -1,16 +1,18 @@
 class Solution {
     public int countDays(int days, int[][] meetings) {
-        Map<Integer, Integer> calendar = new TreeMap<>();
+        TreeMap<Integer, Integer> calendar = new TreeMap<>();
         for (int[] m : meetings) {
             calendar.put(m[0], calendar.getOrDefault(m[0], 0) + 1);
-            calendar.put(m[0], calendar.getOrDefault(m[0], 0) - 1);
+            calendar.put(m[1] + 1, calendar.getOrDefault(m[1] + 1, 0) - 1);
         }
-        int concurrentMeets = 0, freeDays = 0;
-        for (int c : calendar.values()) {
-            concurrentMeets += c;
-            if (concurrentMeets == 0)
-                freeDays++;
+        int concurrentMeets = 0, prevDay = calendar.firstKey(), freeDays = calendar.firstKey();
+        for (Map.Entry<Integer, Integer> e : calendar.entrySet()) {
+            if (concurrentMeets == 0){
+                freeDays += e.getKey() - prevDay;
+            }
+            concurrentMeets += e.getValue();
+            prevDay = e.getKey();
         }
-        return freeDays - 1;
+        return freeDays + days - prevDay;
     }
 }
