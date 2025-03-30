55caa42c93b89c3b7c8b6c67ad27b3784e9dea4a
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-03-30 15:18:27 +0530

    Time: 2 ms (99.38%), Space: 42.2 MB (70.61%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -1,13 +1,15 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
         int[] lastOcc = new int[26];
-        int n = s.length(), prev = -1, window = -1, count = 0;
+        // Arrays.fill(lastOcc, -1);
+        char[] cs = s.toCharArray();
+        int n = cs.length, prev = -1, window = -1, count = 0;
         for (int i = 0; i < n; i++)
-            lastOcc[s.charAt(i) - 'a'] = i;
+            lastOcc[cs[i] - 'a'] = i;
         // System.out.println(Arrays.toString(lastOcc));
         List<Integer> parts = new ArrayList<>();
         for (int i = 0; i < n; i++){
-            window = Math.max(window, lastOcc[s.charAt(i) - 'a']);
+            window = Math.max(window, lastOcc[cs[i] - 'a']);
             if (i == window) {
                 parts.add(i - prev);
                 prev = i;
