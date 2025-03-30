5191288a4d56b8b57acdf4f42bc93007d97c2f45
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-03-30 15:18:20 +0530

    Time: 4 ms (92.83%), Space: 41.8 MB (94.9%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -1,7 +1,6 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
         int[] lastOcc = new int[26];
-        Arrays.fill(lastOcc, -1);
         int n = s.length(), prev = -1, window = -1, count = 0;
         for (int i = 0; i < n; i++)
             lastOcc[s.charAt(i) - 'a'] = i;
