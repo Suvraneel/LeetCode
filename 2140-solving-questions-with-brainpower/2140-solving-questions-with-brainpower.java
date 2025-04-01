c2ff87efa9331b936af3a2acc2f511fc54f77605
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-04-01 22:11:56 +0530

    Time: 5 ms (96.96%), Space: 108 MB (79.64%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -1,11 +1,11 @@
 class Solution {
     public long mostPoints(int[][] questions) {
         int n = questions.length;
-        long[] dp = new long[n];
+        long[] dp = new long[n + 1];
         for (int i = n - 1; i >= 0; i--) {
             int[] q = questions[i];
             int nextValidIdx = i + 1 + q[1];
-            long skip = i < n - 1 ? dp[i + 1] : 0;
+            long skip = dp[i + 1];
             long solve = nextValidIdx < n ? dp[nextValidIdx] : 0;
             dp[i] = Math.max(q[0] + solve, skip);
         }
