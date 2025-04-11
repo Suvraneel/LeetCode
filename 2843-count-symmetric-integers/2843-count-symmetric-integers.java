da24dcbdeff5f8dc32e1cf10689d5816daf5a4c0
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-04-11 17:17:05 +0530

    Time: 27 ms (56.42%), Space: 45 MB (13.42%) - LeetHub

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.java
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.java
@@ -7,18 +7,30 @@ class Solution {
         return count;
     }
 
+    // private boolean isSymmetric(int x) {
+    //     int len = (int) Math.log10(x) + 1, sum = 0;
+    //     if ((len & 1) == 1)
+    //         return false;
+    //     for (int i = 0; i < len / 2; i++) {
+    //         sum += x % 10;
+    //         x /= 10;
+    //     }
+    //     for (int i = len / 2; i < len; i++) {
+    //         sum -= x % 10;
+    //         x /= 10;
+    //     }
+    //     return (sum == 0);
+    // }
+
     private boolean isSymmetric(int x) {
-        int len = (int) Math.log10(x) + 1, sum = 0;
+        String s = String.valueOf(x);
+        int len = s.length(), sum = 0;
         if ((len & 1) == 1)
             return false;
-        for (int i = 0; i < len / 2; i++) {
-            sum += x % 10;
-            x /= 10;
-        }
-        for (int i = len / 2; i < len; i++) {
-            sum -= x % 10;
-            x /= 10;
-        }
+        for (int i = 0; i < len / 2; i++)
+            sum += s.charAt(i);
+        for (int i = len / 2; i < len; i++)
+            sum -= s.charAt(i);
         return (sum == 0);
     }
 }
