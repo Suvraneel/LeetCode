8d4047f0b1c8a6efd1f12dc4c2ee969c74362bba
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-04-17 02:04:56 +0530

    Time: 50 ms (70.43%), Space: 59.7 MB (58.14%) - LeetHub

--- 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.java
+++ 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.java
@@ -1,31 +1,21 @@
 class Solution {
     public long countGood(int[] nums, int k) {
-        int n = nums.length, lt = 0, rt = 0, oldFreq, newFreq;
-        long numPairs = 0, ans = 0;
+        int n = nums.length, numPairs = 0;
+        long ans = 0;
         Map<Integer, Integer> freq = new HashMap<>(); 
-        for(;rt<n; rt++){
-            // expand window
-            oldFreq = freq.getOrDefault(nums[rt], 0);
-            newFreq = oldFreq + 1;
-            freq.put(nums[rt], newFreq);
-            numPairs += getCombinationsCount(newFreq) - getCombinationsCount(oldFreq);
-            if(numPairs>=k){
-                while(numPairs>=k && lt<rt){
-                    oldFreq = freq.getOrDefault(nums[lt], 0);
-                    newFreq = oldFreq - 1;
-                    if(newFreq==0)
-                        freq.remove(nums[lt]);
-                    else freq.put(nums[lt], newFreq);
-                    numPairs += getCombinationsCount(newFreq) - getCombinationsCount(oldFreq);
-                    lt++;
-                }
-                ans += lt;
-                // System.out.println(lt + "\t" + rt + "\t" + ans);
+        for(int lt = 0, rt = -1; lt < n; lt++){
+            while(numPairs<k && rt<n-1){
+                rt++;
+                int oldFreq = freq.getOrDefault(nums[rt], 0);
+                numPairs += oldFreq;
+                freq.put(nums[rt], oldFreq + 1);
             }
+            if(numPairs>=k)
+                ans += n - rt;
+            int oldFreq = freq.get(nums[lt]);
+            freq.put(nums[lt], oldFreq - 1);
+            numPairs -= oldFreq - 1;
         }
         return ans;
-    }
-    private int getCombinationsCount(int n){    // nCr, where r = 2
-        return n * (n-1) / 2;
     }
 }
