0d31a64dd21c882c7d978ccf04aa90db33e91922
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-04-18 21:07:57 +0530

    Time: 1 ms (83.43%), Space: 40.7 MB (93.89%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverse(int x) {
+        int rev = 0, lastValidNum = Integer.MAX_VALUE / 10;
+        boolean negative = x < 0;
+        x = Math.abs(x);
+        while (x > 0) {
+            if (rev > lastValidNum)
+                return 0;
+            rev = rev * 10 + (x % 10);
+            x /= 10;
+        }
+        return negative ? -rev : rev;
+    }
+}
