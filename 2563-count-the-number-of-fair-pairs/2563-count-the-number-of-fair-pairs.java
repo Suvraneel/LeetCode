8a9e2e52480878a839eb3a5d5ab78ba9d640816f
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-04-19 21:11:12 +0530

    Time: 51 ms (56.28%), Space: 57.5 MB (41.82%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
@@ -1,27 +1,47 @@
 class Solution {
-
     public long countFairPairs(int[] nums, int lower, int upper) {
         Arrays.sort(nums);
-        return lower_bound(nums, upper + 1) - lower_bound(nums, lower);
+        int n = nums.length, lt = 1, rt = n - 1;
+        long ans = 0;
+        // System.out.println(Arrays.toString(nums));
+        for (int i = 0; i < n - 1; i++) {
+            rt = findWindowEnd(nums, upper - nums[i], i + 1, rt);
+            if (nums[i] + nums[rt] < lower)
+                continue;
+            lt = findWindowStart(nums, lower - nums[i], i + 1, rt);
+            if (nums[i] + nums[lt] > upper)
+                continue;
+            // System.out.println(nums[i] + "\t" + nums[lt] + "\t" + nums[rt]);
+            ans += rt - lt + 1;
+        }
+        return ans;
     }
 
-    // Calculate the number of pairs with sum less than `value`.
-    private long lower_bound(int[] nums, int value) {
-        int left = 0, right = nums.length - 1;
-        long result = 0;
-        while (left < right) {
-            int sum = nums[left] + nums[right];
-            // If sum is less than value, add the size of window to result and move to the
-            // next index.
-            if (sum < value) {
-                result += (right - left);
-                left++;
-            } else {
-                // Otherwise, shift the right pointer backwards, until we get a valid window.
-                right--;
+    private int findWindowStart(int[] nums, int target, int lt, int rt) {
+        int ans = lt;
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (nums[mid] < target)
+                lt = mid + 1;
+            else {
+                ans = mid;
+                rt = mid - 1;
             }
         }
+        return ans;
+    }
 
-        return result;
+    private int findWindowEnd(int[] nums, int target, int lt, int rt) {
+        int ans = rt;
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (nums[mid] > target)
+                rt = mid - 1;
+            else {
+                ans = mid;
+                lt = mid + 1;
+            }
+        }
+        return ans;
     }
 }
