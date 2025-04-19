844f19a6ab89b34d4283602ff16e53b49403fb20
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-04-20 04:40:32 +0530

    Time: 1 ms (48.8%), Space: 57.3 MB (65.88%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -4,7 +4,7 @@ class Solution {
         k %= n;
         int[] temp = new int[k];
         for (int i = 0; i < k; i++)
-            temp[i] = nums[(n + i - k) % n];
+            temp[i] = nums[n + i - k];
         for (int i = n - 1; i >= k; i--)
             nums[i] = nums[i - k];
         for (int i = 0; i < k; i++)
