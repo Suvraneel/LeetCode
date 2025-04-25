baf5ab39cd0c03e145384e5990ab160a6d689b83
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-04-25 15:10:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2845-count-of-interesting-subarrays/2845-count-of-interesting-subarrays.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
+        int n = nums.size(), prefix = 0;
+        long ans = 0;
+        Map<Integer, Integer> moduloBucket = new HashMap<>();
+        moduloBucket.put(0, 1);
+        for (int i = 0; i < n; i++) {
+            if (nums.get(i) % modulo == k)
+                prefix++;
+            ans += moduloBucket.get((prefix - k + modulo) % modulo);
+            moduloBucket.put(prefix % modulo, moduloBucket.getOrDefault(prefix % modulo, 0) + 1);
+        }
+        return ans;
+    }
+}
