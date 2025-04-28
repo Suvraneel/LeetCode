4edda949fd468bbe1a83c856bb9a23272fad11a7
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-04-28 22:03:30 +0530

    Time: 2 ms (100%), Space: 61.6 MB (48.39%) - LeetHub

--- 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.java
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.java
@@ -1,6 +1,5 @@
 class Solution {
     public long countSubarrays(int[] nums, long k) {
-        // (prefix2 - prefix1) * (i2 - i1) < k
         long prefixSum = 0, ans = 0;
         for (int lt = 0, rt = 0; rt < nums.length; rt++) {
             prefixSum += nums[rt];
