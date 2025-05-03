3e53492b28e6bc19d2d2b1cb0ee30c977ab06576
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-05-03 17:57:23 +0530

    Time: 4 ms (84.8%), Space: 50.6 MB (34.67%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -1,23 +1,20 @@
 class Solution {
-    public int minDominoRotations(int[] tops, int[] bottoms) {
-        int n = tops.length, a = tops[0], b = bottoms[0], x;
+    private int checkValid(int[] tops, int[] bottoms, int x) {
+        int n = tops.length, cTop = 0, cBot = 0;
         for (int i = 0; i < n; i++) {
-            if (tops[i] != a && bottoms[i] != a)
-                a = -1;
-            if (tops[i] != b && bottoms[i] != b)
-                b = -1;
-        }
-        x = Math.max(a, b);
-        if (x == -1)
-            return x;
-        a = 0;
-        b = 0;
-        for (int i = 0; i < n; i++) {
+            if (tops[i] != x && bottoms[i] != x)
+                return -1;
             if (tops[i] == x)
-                a++;
+                cTop++;
             if (bottoms[i] == x)
-                b++;
+                cBot++;
         }
-        return Math.min(n - a, n - b);
+        return Math.min(n - cTop, n - cBot);
+    }
+
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int n = tops.length, a = tops[0], b = bottoms[0];
+        int ansA = checkValid(tops, bottoms, a), ansB = checkValid(tops, bottoms, b);
+        return Math.max(ansA, ansB);
     }
 }
