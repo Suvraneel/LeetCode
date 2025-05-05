5b44bfa1bdc2196ac6797ef6a099633f25228460
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-05-06 04:24:42 +0530

    Time: 3 ms (71.95%), Space: 50.2 MB (5.05%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,13 +1,34 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> m = new HashMap<>();
+        int n = nums.length, major1 = Integer.MIN_VALUE, major2 = Integer.MIN_VALUE, count1 = 0, count2 = 0;
         List<Integer> ans = new ArrayList<>();
+        for (int i : nums) {
+            if (count2 == 0 && i != major1) { // moore voting but ignore major1 (since both candidate shouldnt be same)
+                major2 = i;
+                count2 = 1;
+            } else if (count1 == 0 && i != major2) {
+                major1 = i;
+                count1 = 1;
+            } else if (i == major1)
+                count1++;
+            else if (i == major2)
+                count2++;
+            else {
+                count1--;
+                count2--;
+            }
+        }
+        count1 = 0;
+        count2 = 0;
         for (int i : nums)
-            m.put(i, m.getOrDefault(i, 0) + 1);
-        int majorityThreshold = nums.length / 3;
-        for (Map.Entry<Integer, Integer> e : m.entrySet())
-            if (e.getValue() > majorityThreshold)
-                ans.add(e.getKey());
+            if (i == major1)
+                count1++;
+            else if (i == major2)
+                count2++;
+        if (count1 > n / 3)
+            ans.add(major1);
+        if (count2 > n / 3)
+            ans.add(major2);
         return ans;
     }
 }
