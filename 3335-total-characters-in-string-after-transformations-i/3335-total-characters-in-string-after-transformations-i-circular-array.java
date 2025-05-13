fc8957c4a4976bc4a0d42a08250e277213e2b92f
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-05-14 00:33:01 +0530

    Time: 24 ms (96.2%), Space: 45.8 MB (41.77%) - LeetHub

--- 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i-circular-array.java
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i-circular-array.java
@@ -1,17 +1,16 @@
 class Solution {
     public int lengthAfterTransformations(String s, int t) {
-        int MOD = 1_000_000_007;
-        long len = s.length();
-        long[] freq = new long[26];
+        int[] freq = new int[26];
         for (char c : s.toCharArray())
             freq[c - 'a']++;
-        int Zs = 25, Bs = 0;
+        int MOD = 1_000_000_007, len = s.length(), Zs = 25, Bs = 0;
+        // idx ptrs for chars Z and B (B=0 bcz prev A contributes to B, prev B contributes to C instead)
         for (int i = 0; i < t; i++) {
             len = (len + freq[Zs]) % MOD;
-            freq[Bs] = (freq[Bs] + freq[Zs]) % MOD;
-            Zs = (26 + Zs - 1) % 26;
-            Bs = (Zs + 1) % 26;
+            freq[Bs] = (freq[Bs] + freq[Zs]) % MOD; // so init with freq('a'), then add 'z' contribution 
+            Zs = (26 + Zs - 1) % 26; // left shift Z ptr circularly, to simulate A->B, B->C & so on.
+            Bs = (Zs + 1) % 26; // the char right after Z contributes to B
         }
-        return (int) len;
+        return len;
     }
 }
