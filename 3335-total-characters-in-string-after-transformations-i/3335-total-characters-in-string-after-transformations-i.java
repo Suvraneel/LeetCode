4bc9be407380fbdc91932cf2911a7104ed86b7c4
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-05-14 00:23:58 +0530

    Time: 24 ms (96.2%), Space: 45.8 MB (29.11%) - LeetHub

--- 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.java
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.java
@@ -1,20 +1,16 @@
 class Solution {
     public int lengthAfterTransformations(String s, int t) {
         int MOD = 1_000_000_007;
+        long len = 0;
         long[] freq = new long[26];
         for (char c : s.toCharArray())
             freq[c - 'a']++;
-        while (t-- > 0) {
-            long temp = freq[25];
-            freq[25] = 0;
-            for (int i = 24; i >= 0; i--) {
-                freq[i + 1] = freq[i];
-                freq[i] = 0;
-            }
-            freq[0] = temp;
-            freq[1] = (freq[1] + temp) % MOD;
+        int Zs = 25, Bs = 0;
+        for (int i = 0; i < t; i++) {
+            freq[Bs] = (freq[Bs] + freq[Zs]) % MOD;
+            Zs = (26 + Zs - 1) % 26;
+            Bs = (Zs + 1) % 26;
         }
-        long len = 0;
         for (long f : freq)
             len = (len + f) % MOD;
         return (int) len;
