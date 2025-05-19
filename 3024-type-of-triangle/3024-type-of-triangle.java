64fc76db2e14ba78cfbf95edc1b6f84d83db5fdc
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-05-20 01:48:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String triangleType(int[] nums) {
+        Arrays.sort(nums);
+        if (nums[0] + nums[1] <= nums[2])
+            return "none";
+        if (nums[0] == nums[1]) {
+            if (nums[1] == nums[2])
+                return "equilateral";
+            return "isosceles";
+        }
+        return "scalene";
+    }
+}
