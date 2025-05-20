9d85a5a1ba61da8b7c48eb6be74a1126d9a85f93
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-05-21 02:10:10 +0530

    Time: 2 ms (100%), Space: 95.9 MB (30.86%) - LeetHub

--- 3355-zero-array-transformation-i/3355-zero-array-transformation-i.java
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
-        int n = nums.length;
+        int n = nums.length, prefixSum = 0;
         int[] diffArr = new int[n + 1];
         for (int[] q : queries) {
             diffArr[q[0]]--;
             diffArr[q[1] + 1]++;
         }
         for (int i = 0; i < n; i++) {
-            diffArr[i + 1] += diffArr[i];
-            if (nums[i] + diffArr[i] > 0)
+            prefixSum += diffArr[i];
+            if (nums[i] + prefixSum > 0)
                 return false;
         }
         return true;
