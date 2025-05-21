30e621c0c95482e6dd3c619e6440c4fdd48d31c8
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-05-22 00:19:02 +0530

    Time: 1 ms (72.83%), Space: 45.6 MB (66.55%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,19 +1,17 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
-        Set<Integer> Is = new HashSet<>();
-        Set<Integer> Js = new HashSet<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    Is.add(i);
-                    Js.add(j);
+        boolean[] row0 = new boolean[m];
+        boolean[] col0 = new boolean[n];
+        for(int i=0; i<m; i++)
+            for(int j=0; j<n; j++)
+                if(matrix[i][j]==0){
+                    row0[i] = true;
+                    col0[j] = true;
                 }
-            }
-        }
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (Is.contains(i) || Js.contains(j))
+        for(int i=0; i<m; i++)
+            for(int j=0; j<n; j++)
+                if(row0[i] || col0[j])
                     matrix[i][j] = 0;
     }
 }
