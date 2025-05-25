e6c00d34dc2a6690a15a55dc6c341631f9cc9fec
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-05-25 18:52:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,25 +1,23 @@
 class Solution {
     public int longestPalindrome(String[] words) {
-        Map<String, Integer> map = new HashMap<>();
-        int count = 0;
-        for (String w : words) {
-            String rev = w.charAt(1) + "" + w.charAt(0);
-            if (map.containsKey(rev)) {
-                map.put(rev, map.get(rev) - 1);
-                if (map.get(rev) == 0)
-                    map.remove(rev);
-                count += 2;
-            } else
-                map.put(w, map.getOrDefault(w, 0) + 1);
+        int[][] freq = new int[26][26];
+        for (String w : words)
+            freq[w.charAt(0) - 'a'][w.charAt(1) - 'a']++;
+        int usedWords = 0, unusedPals = 0;
+        for (int i = 0; i < 26; i++) {
+            usedWords += freq[i][i] / 2;
+            unusedPals += freq[i][i] % 2;
+            freq[i][i] = 0;
         }
-        // System.out.println(map);
-        for (String w : map.keySet()) {
-            String rev = w.charAt(1) + "" + w.charAt(0);
-            if (w.equals(rev)) {
-                count++;
-                break;
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 26; j++) {
+                usedWords += Math.min(freq[i][j], freq[j][i]) * 2;
+                freq[i][j] = 0;
+                freq[j][i] = 0;
             }
         }
-        return count * 2;
+        if (unusedPals > 0)
+            usedWords++;
+        return usedWords * 2;
     }
 }
