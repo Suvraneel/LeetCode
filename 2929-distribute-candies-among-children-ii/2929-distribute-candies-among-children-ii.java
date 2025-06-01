ced6d89758fefb386ade333656ac1d9c0a83efdb
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-06-02 02:53:01 +0530

    Time: 0 ms (100%), Space: 41.3 MB (14.29%) - LeetHub

--- 2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.java
+++ 2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.java
@@ -1,11 +1,13 @@
 class Solution {
     public long distributeCandies(int n, int limit) {
-        return combination(n + 2, 2) - 3 * combination(n + 2 - (limit + 1), 2)
-                + 3 * combination(n + 2 - 2 * (limit + 1), 2)
-                - combination(n + 2 - 3 * (limit + 1), 2);
+        // return combination(n + 2, 2) - 3 * combination(n + 2 - (limit + 1), 2)
+        //         + 3 * combination(n + 2 - 2 * (limit + 1), 2)
+        //         - combination(n + 2 - 3 * (limit + 1), 2);
+        return combination(n + 2) - 3 * combination(n - limit + 1) + 3 * combination(n - 2 * limit)
+                - combination(n - 3 * limit - 1);
     }
 
-    private long combination(int n, int r) { // nCr, where r=2 => nC2
+    private long combination(int n) { // nCr, where r=2 => nC2
         if (n < 2) // cant choose 2 items from a collection containing less that n objects
             return 0;
         return (long) n * (n - 1) / 2;
