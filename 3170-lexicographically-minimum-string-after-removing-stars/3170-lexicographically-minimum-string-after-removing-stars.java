5ffc9f95e260949e69366a98ab69fef536f81d2d
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-06-08 02:26:03 +0530

    Time: 197 ms (55.75%), Space: 51 MB (29.31%) - LeetHub

--- 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.java
+++ 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.java
@@ -4,12 +4,11 @@ class Solution {
         char[] cs = s.toCharArray();
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
         for (int i = 0; i < n; i++) {
-            if (cs[i] == '*') {
-                cs[pq.poll()[1]] = '.';
-                cs[i] = '.';
-            } else
+            if (cs[i] == '*')
+                cs[pq.poll()[1]] = '*';
+            else
                 pq.offer(new int[] { cs[i] - 'a', i });
         }
-        return new String(cs).replace(".", "");
+        return new String(cs).replace("*", "");
     }
 }
