b55df635259c98aeed149ab65ea0f52801e9843f
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-06-15 18:03:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maxDiff(int num) {
+        char[] cs = String.valueOf(num).toCharArray();
+        char k = 'k', l = 'l';
+        int max = 0, min = 0;
+        for (int i = 0; i < cs.length; i++) {
+            max *= 10;
+            if (k == 'k' && cs[i] != '9') {
+                k = cs[i];
+                max += 9;
+            } else if (cs[i] == k)
+                max += 9;
+            else
+                max += cs[i] - '0';
+        }
+        if (cs[0] == '1') {
+            for (int i = 0; i < cs.length; i++) {
+                min *= 10;
+                if (l == 'l' && cs[i] != '1') {
+                    l = cs[i];
+                    // min += 0;
+                } else if (cs[i] == l)
+                    ; // min += 0;
+                else
+                    min += cs[i] - '0';
+            }
+        } else {
+            l = cs[0];
+            for (int i = 0; i < cs.length; i++) {
+                min *= 10;
+                if (cs[i] == l)
+                    min += 1;
+                else
+                    min += cs[i] - '0';
+            }
+        }
+        // System.out.println(max + "\t" + min);
+        return max - min;
+    }
+}
