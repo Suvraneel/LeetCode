5a948f5f9712722b2b03ebdbd1a6900460fe926e
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-06-15 19:03:47 +0530

    Time: 1 ms (92.38%), Space: 40.5 MB (73.33%) - LeetHub

--- 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.java
+++ 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.java
@@ -3,16 +3,18 @@ class Solution {
         char[] cs = String.valueOf(num).toCharArray();
         char k = 'k', l = 'l';
         int max = 0, min = 0;
+        for (int i = 0; i < cs.length; i++) {
+            max *= 10;
+            if (k == 'k' && cs[i] != '9') {
+                k = cs[i];
+                max += 9;
+            } else if (cs[i] == k)
+                max += 9;
+            else
+                max += cs[i] - '0';
+        }
         if (cs[0] == '1') {
             for (int i = 0; i < cs.length; i++) {
-                max *= 10;
-                if (k == 'k' && cs[i] != '9') {
-                    k = cs[i];
-                    max += 9;
-                } else if (cs[i] == k)
-                    max += 9;
-                else
-                    max += cs[i] - '0';
                 min *= 10;
                 if (l == 'l' && cs[i] != '1' && cs[i] != '0') {
                     l = cs[i];
@@ -25,14 +27,6 @@ class Solution {
         } else {
             l = cs[0];
             for (int i = 0; i < cs.length; i++) {
-                max *= 10;
-                if (k == 'k' && cs[i] != '9') {
-                    k = cs[i];
-                    max += 9;
-                } else if (cs[i] == k)
-                    max += 9;
-                else
-                    max += cs[i] - '0';
                 min *= 10;
                 if (cs[i] == l)
                     min += 1;
