c42c9a92dca528da767dce2d892418bf6a75c109
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-06-19 01:21:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
@@ -6,8 +6,7 @@ class Solution {
         for (int i = 0; i < nums.length; i += 3) {
             if (nums[i + 2] - nums[i] > k)
                 return new int[0][0];
-            for (int j = 0; j < 3; j++)
-                ans[i / 3][j] = nums[i + j];
+            ans[i / 3][0] = nums[i];
         }
         return ans;
     }
