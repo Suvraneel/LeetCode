d1a087ef999260dd860de9a3c35c2abf5372fed8
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-06-25 21:24:03 +0530

    Time: 5 ms (28.35%), Space: 47.8 MB (44.6%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,23 +1,14 @@
 class Solution {
-    int INF = Integer.MAX_VALUE, m, n;
-
     public int minPathSum(int[][] grid) {
-        m = grid.length;
-        n = grid[0].length;
-        int[][] minCost = new int[m][n];
-        for (int i = 0; i < m; i++)
-            Arrays.fill(minCost[i], INF);
-        minCost[m - 1][n - 1] = grid[m - 1][n - 1];
-        dfs(grid, 0, 0, minCost);
-        return minCost[0][0];
-    }
-
-    private int dfs(int[][] grid, int i, int j, int[][] minCost) {
-        if (i >= m || j >= n)
-            return Integer.MAX_VALUE;
-        if (minCost[i][j] != INF)
-            return minCost[i][j];
-        return minCost[i][j] = grid[i][j] +
-                Math.min(dfs(grid, i + 1, j, minCost), dfs(grid, i, j + 1, minCost));
+        int m = grid.length, n = grid[0].length;
+        for (int i = m - 1; i >= 0; i--)
+            for (int j = n - 1; j >= 0; j--)
+                grid[i][j] += i == m - 1 && j == n - 1 ? 0
+                        : i == m - 1 ? grid[i][j + 1]
+                                : j == n - 1 ? grid[i + 1][j]
+                                        : Math.min(grid[i + 1][j], grid[i][j + 1]);
+        // for (int i = 0; i < m; i++)
+        //     System.out.println(Arrays.toString(grid[i]));
+        return grid[0][0];
     }
 }
