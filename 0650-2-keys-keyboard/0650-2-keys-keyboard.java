9c21afa28b1c2054b4d09d68df765ab94c26084b
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-06-26 04:27:24 +0530

    Time: 28 ms (13.76%), Space: 41.2 MB (23.82%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,23 +1,14 @@
 class Solution {
-    int n;
-
     public int minSteps(int n) {
-        if (n == 1)
-            return 0;
-        this.n = n;
-        int[][] dp = new int[n + 1][n / 2 + 1];
-        return 1 + solve(1, 1, dp);
-    }
-
-    private int solve(int total, int clipboard, int[][] dp) {
-        if (total > n)
-            return 1000;
-        if (total == n)
-            return 0;
-        if (dp[total][clipboard] != 0)
-            return dp[total][clipboard];
-        return dp[total][clipboard] = Math.min(
-                solve(total + clipboard, clipboard, dp) + 1,
-                solve(total * 2, total, dp) + 2);
+        int[] minOps = new int[n + 1];
+        Arrays.fill(minOps, n + 1);
+        minOps[1] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i / 2; j++)
+                if (i % j == 0)
+                    minOps[i] = Math.min(minOps[i], minOps[j] + i / j);
+        }
+        // System.out.println(Arrays.toString(minOps));
+        return minOps[n];
     }
 }
