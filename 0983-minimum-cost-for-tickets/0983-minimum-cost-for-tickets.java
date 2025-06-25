984fe6f4397a60b71b4c901e4e7e61a82e0c200d
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-06-26 03:01:38 +0530

    Time: 2 ms (43.34%), Space: 41.9 MB (37.53%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,22 +1,26 @@
 class Solution {
-    int[] passes = { 1, 7, 30 };
+    int[] minCost = new int[366];
+    int INF = 1001;
 
     public int mincostTickets(int[] days, int[] costs) {
-        int[] memo = new int[days.length + 1];
-        return solve(days, costs, 0, memo);
+        Arrays.fill(minCost, INF);
+        int[] pass = { 1, 7, 30 };
+        Set<Integer> need = new HashSet<>();
+        for (int d : days)
+            need.add(d);
+        dfs(need, days, costs, 0);
+        return minCost[days[0]];
     }
 
-    private int solve(int[] days, int[] costs, int dayIdx, int[] memo) {
-        if (dayIdx == days.length)
+    private int dfs(Set<Integer> need, int[] days, int[] costs, int day) {
+        if (day > days[days.length - 1])
             return 0;
-        if (memo[dayIdx] != 0)
-            return memo[dayIdx];
-        int $ = Integer.MAX_VALUE, idx = dayIdx + 1;
-        for (int i = 0; i < 3; i++) {
-            while (idx < days.length && days[idx] < days[dayIdx] + passes[i])
-                idx++;
-            $ = Math.min($, costs[i] + solve(days, costs, idx, memo));
-        }
-        return memo[dayIdx] = $;
+        if (!need.contains(day))
+            return dfs(need, days, costs, day + 1);
+        if (minCost[day] != INF)
+            return minCost[day];
+        minCost[day] = Math.min(dfs(need, days, costs, day + 1) + costs[0], Math.min(
+                dfs(need, days, costs, day + 7) + costs[1], dfs(need, days, costs, day + 30) + costs[2]));
+        return minCost[day];
     }
 }
