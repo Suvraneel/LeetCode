e903dd2d1d65d5ccbb206e7ee05e4801e12e5127
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-07-08 04:51:39 +0530

    Time: 69 ms (34.49%), Space: 75.8 MB (34.61%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.java
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.java
@@ -1,9 +1,9 @@
 class Solution {
     public int maxEvents(int[][] events) {
         Arrays.sort(events, (a, b) -> a[0] - b[0]);
-        int n = events.length, ans = 0;
+        int n = events.length, ans = 0, maxDay = Arrays.stream(events).mapToInt(e -> e[1]).max().orElse(0);
         Queue<Integer> minHeap = new PriorityQueue<>();
-        for (int d = 0, e = 0; d < 10e5; d++) {
+        for (int d = 0, e = 0; d <= maxDay; d++) {
             while (e < n && events[e][0] <= d)
                 minHeap.offer(events[e++][1]);
             while (!minHeap.isEmpty() && minHeap.peek() < d) // missed events
