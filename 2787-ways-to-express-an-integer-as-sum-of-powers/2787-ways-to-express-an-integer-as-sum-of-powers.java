4d645059630532f1f4be8135f60d03231633316c
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-08-12 23:09:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.java
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.java
@@ -4,16 +4,16 @@ class Solution {
     public int numberOfWays(int n, int x) {
         // Max term: n1^x=n  => xlogn1=logn => n1 = 10^(logn/x)
         int rt = (int) Math.ceil(Math.pow(10, Math.log10(n) / x));
-        Integer memo[][] = new Integer[n + 1][rt + 1];
+        int memo[][] = new int[n + 1][rt + 1];
         return solve(n, x, rt, memo);
     }
 
-    private int solve(int n, int x, int rt, Integer[][] memo) {
+    private int solve(int n, int x, int rt, int[][] memo) {
         if (n == 0)
             return 1; // 1 way
         else if (n < 0 || rt == 0)
             return 0; // invalid; backtrack
-        if (memo[n][rt] != null)
+        if (memo[n][rt] != 0)
             return memo[n][rt];
         return memo[n][rt] = (solve(n - (int) Math.pow(rt, x), x, rt - 1, memo) + solve(n, x, rt - 1, memo)) % MOD;
     }
