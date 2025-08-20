a813fa298c37fafa635bec9ae4b2d7e62d5a3e3b
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-08-20 20:25:14 +0530

    Time: 55 ms (5.09%), Space: 55.5 MB (29.78%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,21 +1,35 @@
 class Solution {
+    private void print(int[][] mat) {
+        for (int i = 0; i < mat.length; i++)
+            System.out.println(Arrays.toString(mat[i]));
+        System.out.println();
+    }
 
     public int countSquares(int[][] matrix) {
-        int row = matrix.length, col = matrix[0].length;
-        int[][] dp = new int[row + 1][col + 1];
-        int ans = 0;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (matrix[i][j] == 1) {
-                    dp[i + 1][j + 1] = Math.min(
-                        Math.min(dp[i][j + 1], dp[i + 1][j]),
-                        dp[i][j]
-                    ) +
-                    1;
-                    ans += dp[i + 1][j + 1];
+        int m = matrix.length, n = matrix[0].length, maxDim = Math.min(m, n), count = 0;
+        // generate prefix sum matrix
+        int[][] prefix = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++)
+                prefix[i + 1][j + 1] = sum += matrix[i][j];
+        }
+        // print(prefix);
+        for (int j = 0; j <= n; j++)
+            for (int i = 0; i < m; i++)
+                prefix[i + 1][j] += prefix[i][j];
+        // print(prefix);
+        for (int k = 1; k <= maxDim; k++) {
+            System.out.println("k->" + k);
+            for (int i = k; i <= m; i++) {
+                for (int j = k; j <= n; j++) {
+                    // System.out.print(prefix[i][j] - prefix[i - k][j] - prefix[i][j - k] + prefix[i - k][j - k] + "\t");
+                    if (prefix[i][j] - prefix[i - k][j] - prefix[i][j - k] + prefix[i - k][j - k] == k * k)
+                        count++;
                 }
+                // System.out.println();
             }
         }
-        return ans;
+        return count;
     }
 }
