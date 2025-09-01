277fce30f69ea2c2b8c671ae8dbac198ae9649ba
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-09-02 05:21:49 +0530

    Time: 361 ms (52.82%), Space: 96.4 MB (32.82%) - LeetHub

--- 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.java
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.java
@@ -1,15 +1,16 @@
 class Solution {
-    private double calcGain(double[] a) {
-        return (a[0] + 1) / (a[1] + 1) - a[0] / a[1];
+    private double calcGain(double numerator, double denominator) {
+        return (numerator + 1) / (denominator + 1) - numerator / denominator;
     }
 
     public double maxAverageRatio(int[][] classes, int extraStudents) {
-        Queue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(calcGain(b), calcGain(a)));
-        for (int[] c : classes)
-            pq.offer(new double[] { c[0], c[1] });
+        Queue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[2], a[2])); // compare gains
+        for (int[] c : classes) {
+            pq.offer(new double[] { c[0], c[1], calcGain(c[0], c[1]) });
+        }
         while (extraStudents-- > 0) {
             double[] top = pq.poll();
-            pq.offer(new double[] { top[0] + 1, top[1] + 1 });
+            pq.offer(new double[] { top[0] + 1, top[1] + 1, calcGain(top[0] + 1, top[1] + 1) });
         }
         double avgRatio = 0;
         while (!pq.isEmpty()) {
