3f9ca4eddf60d27789de5945d5a0e68e7592d7a3
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-09-13 03:28:27 +0530

    Time: 22 ms (8.93%), Space: 45.7 MB (45.15%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
@@ -1,12 +1,5 @@
 class Solution {
     public boolean doesAliceWin(String s) {
-        // If num of vowels in s is odd, Alice removes entire s & takes the win
-        // If num of vowels in s is zero, Alice cant make a move & so Bob wins
-        /** If num of vowels in s is even but not zero, Alice will rm odd num of chars (ie, vowelCt - 1),
-        * Bob may lose in round 1 if no consonants left, 
-        * else he will lose in round 2 when Alice rms the remaining vowel
-        */
-        // So, in a nutshell, Alice can only lose when no. of vowels in s is exactly zero.
         return s.chars().anyMatch(c -> "aeiou".indexOf(c) != -1);
     }
 }
