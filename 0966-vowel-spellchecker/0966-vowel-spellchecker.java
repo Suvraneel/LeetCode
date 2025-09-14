c0fb83a068988a9cb1f5c7256fd88801853fd9df
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-09-15 03:58:10 +0530

    Time: 56 ms (18.66%), Space: 53 MB (9.57%) - LeetHub

--- 0966-vowel-spellchecker/0966-vowel-spellchecker.java
+++ 0966-vowel-spellchecker/0966-vowel-spellchecker.java
@@ -1,21 +1,31 @@
 class Solution {
     public String[] spellchecker(String[] wordlist, String[] queries) {
-        Map<String, String> mapExact = new HashMap<>();
+        Set<String> exactMatch = new HashSet<>();
         Map<String, String> mapCapitalize = new HashMap<>();
         Map<String, String> mapError = new HashMap<>();
-        int m = wordlist.length, n = queries.length;
+        int m = wordlist.length, n = queries.length, j = 0;
         for (int i = m - 1; i >= 0; i--) {
-            mapExact.put(wordlist[i], wordlist[i]);
+            exactMatch.add(wordlist[i]);
             mapCapitalize.put(wordlist[i].toLowerCase(), wordlist[i]);
             mapError.put(wordlist[i].toLowerCase().replaceAll("[aeiou]", "*"), wordlist[i]);
         }
         String[] ans = new String[n];
-        for (int i = 0; i < n; i++) {
-            String q = queries[i];
-            ans[i] = mapExact.getOrDefault(q,
-                    mapCapitalize.getOrDefault(q.toLowerCase(),
-                            mapError.getOrDefault(q.toLowerCase().replaceAll("[aeiou]", "*"),
-                                    "")));
+        for (String q : queries) {
+            if (exactMatch.contains(q)) {
+                ans[j++] = q;
+                continue;
+            }
+            String ql = q.toLowerCase();
+            if (mapCapitalize.containsKey(ql)) {
+                ans[j++] = mapCapitalize.get(ql);
+                continue;
+            }
+            String qe = q.toLowerCase().replaceAll("[aeiou]", "*");
+            if (mapError.containsKey(qe)) {
+                ans[j++] = mapError.get(qe);
+                continue;
+            } else
+                ans[j++] = "";
         }
         return ans;
     }
