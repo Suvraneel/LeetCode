b73cc23688efa408be6ffd75674355c4f37c04cc
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-09-15 03:42:31 +0530

    Time: 65 ms (10.05%), Space: 52.5 MB (23.44%) - LeetHub

--- 0966-vowel-spellchecker/0966-vowel-spellchecker.java
+++ 0966-vowel-spellchecker/0966-vowel-spellchecker.java
@@ -3,10 +3,11 @@ class Solution {
         Map<String, String> mapExact = new HashMap<>();
         Map<String, String> mapCapitalize = new HashMap<>();
         Map<String, String> mapError = new HashMap<>();
-        for (String w: wordlist) {
-            mapExact.put(w, wordlist[i]);
-            mapCapitalize.put(w.toLowerCase(), wordlist[i]);
-            mapError.put(w.toLowerCase().replaceAll("[aeiou]", "*"), wordlist[i]);
+        int m = wordlist.length, n = queries.length;
+        for (int i = m - 1; i >= 0; i--) {
+            mapExact.put(wordlist[i], wordlist[i]);
+            mapCapitalize.put(wordlist[i].toLowerCase(), wordlist[i]);
+            mapError.put(wordlist[i].toLowerCase().replaceAll("[aeiou]", "*"), wordlist[i]);
         }
         String[] ans = new String[n];
         for (int i = 0; i < n; i++) {
