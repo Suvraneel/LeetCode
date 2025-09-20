7b80894e340a71c874daa9673aa18bfdc9757807
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-09-21 05:28:26 +0530

    Time: 206 ms (20.54%), Space: 151.8 MB (56.25%) - LeetHub

--- 3508-implement-router/3508-implement-router.java
+++ 3508-implement-router/3508-implement-router.java
@@ -1,80 +1,67 @@
-public class Packet {
-    int src;
-    int dest;
-    int tsp;
-
-    public Packet(int source, int destination, int timestamp) {
-        src = source;
-        dest = destination;
-        tsp = timestamp;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        Packet o = (Packet) other;
-        return this.src == o.src && this.dest == o.dest && this.tsp == o.tsp;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(src, dest, tsp);
-    }
-}
-
 class Router {
-    int memCap;
-    LinkedHashSet<Packet> q;
-    TreeMap<Integer, Map<Integer, Integer>> tspBasedDestMap;
+    Map<List<Integer>, Integer> mpp = new HashMap<>(); // to track duplicates
+    Queue<List<Integer>> queue = new LinkedList<>(); // to store packets in FIFO order
+    Map<Integer, List<Integer>> timestamps = new HashMap<>(); // for timestamps tracking
+    Map<Integer, Integer> st = new HashMap<>();
+    int maxSize = 0; // maxSize allowed
 
-    public Router(int memoryLimit) {
-        q = new LinkedHashSet<>();
-        tspBasedDestMap = new TreeMap<>();
-        memCap = memoryLimit;
+    public Router(int memoryLimit) { 
+        maxSize = memoryLimit; 
     }
 
     public boolean addPacket(int source, int destination, int timestamp) {
-        Packet newPacket = new Packet(source, destination, timestamp);
-        if (q.contains(newPacket))
+        List<Integer> packet = Arrays.asList(source, destination, timestamp);
+        // checking for duplicate
+        if (mpp.containsKey(packet))
             return false;
-        if (q.size() == memCap) {
-            Iterator<Packet> it = q.iterator();
-            Packet top = it.next();
-            tspBasedDestMap.get(top.tsp).put(top.dest, tspBasedDestMap.get(top.tsp).get(top.dest) - 1);
-            it.remove();
+        if (queue.size() == maxSize) { // remove the first element if queue is full
+            List<Integer> res = queue.poll();
+            mpp.remove(res);
+            int temp = res.get(1);
+            st.put(temp, st.getOrDefault(temp, 0) + 1);
         }
-        q.add(newPacket);
-        tspBasedDestMap.putIfAbsent(timestamp, new HashMap<>());
-        tspBasedDestMap.get(timestamp).put(destination,
-                tspBasedDestMap.get(timestamp).getOrDefault(destination, 0) + 1);
+        queue.offer(packet);
+        mpp.put(packet, 1);
+        timestamps.computeIfAbsent(destination, k -> new ArrayList<>()).add(timestamp);
         return true;
     }
 
     public int[] forwardPacket() {
-        if (q.isEmpty())
-            return new int[0];
-        Iterator<Packet> it = q.iterator();
-        Packet top = it.next();
-        tspBasedDestMap.get(top.tsp).put(top.dest, tspBasedDestMap.get(top.tsp).get(top.dest) - 1);
-        it.remove();
-        return new int[] { top.src, top.dest, top.tsp };
+        if(queue.isEmpty()) return new int[0];
+        List<Integer> res = queue.poll();
+        mpp.remove(res);
+        int temp = res.get(1);
+        st.put(temp, st.getOrDefault(temp, 0) + 1);
+        return new int[]{res.get(0), res.get(1), res.get(2)};
     }
 
     public int getCount(int destination, int startTime, int endTime) {
-        int matchCount = 0;
-        NavigableMap<Integer, Map<Integer, Integer>> tailMap = tspBasedDestMap.tailMap(startTime, true);
-        for (Map.Entry<Integer, Map<Integer, Integer>> e : tailMap.entrySet()) {
-            if (e.getKey() > endTime)
-                break;
-            matchCount += e.getValue().getOrDefault(destination, 0);
+        if(!timestamps.containsKey(destination))
+            return 0;
+        List<Integer> p = timestamps.get(destination);
+        int temp = st.getOrDefault(destination, 0);
+        int right = lowerBound(p, startTime, temp);
+        int left = upperBound(p, endTime, temp);
+        return left - right;
+    }
+
+    private int lowerBound(List<Integer> p, int target, int start) {
+        int l = start, r = p.size();
+        while(l < r) {
+            int mid = (l + r) / 2;
+            if(p.get(mid) < target) l = mid + 1;
+            else r = mid;
         }
-        return matchCount;
+        return l;
+    }
+
+    private int upperBound(List<Integer> p, int target, int start) {
+        int l = start, r = p.size();
+        while(l < r) {
+            int mid = (l + r) / 2;
+            if(p.get(mid) <= target) l = mid + 1;
+            else r = mid;
+        }
+        return l;
     }
 }
-
-/**
- * Your Router object will be instantiated and called as such:
- * Router obj = new Router(memoryLimit);
- * boolean param_1 = obj.addPacket(source,destination,timestamp);
- * int[] param_2 = obj.forwardPacket();
- * int param_3 = obj.getCount(destination,startTime,endTime);
- */
