e7085bcee836149607be78ec9c8cd8d60f1734fb
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-09-25 03:34:41 +0530

    Time: 7 ms (6.09%), Space: 41.6 MB (8.27%) - LeetHub

---
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+        StringBuilder quotient = new StringBuilder();
+        long dividend = numerator, divisor = denominator;
+        if (dividend < 0 || divisor < 0){
+            if(dividend * divisor < 0)
+                quotient.append("-");
+            dividend = Math.abs(dividend);
+            divisor = Math.abs(divisor);
+        }
+        Map<Long, Integer> repeatingDividend = new HashMap<>(); // sequence -> decimal-th place
+        quotient.append(dividend / divisor);
+        dividend = (dividend % divisor) * 10;
+        if (dividend == 0)
+            return quotient.toString();
+        quotient.append(".");
+        while (dividend > 0) {
+            if (dividend >= divisor) {
+                quotient.append(dividend / divisor);
+                dividend = (dividend % divisor) * 10;
+            } else {
+                quotient.append("0");
+                dividend *= 10;
+            }
+            if (repeatingDividend.containsKey(dividend)) {
+                return quotient.substring(0, repeatingDividend.get(dividend)) + "(" +
+                        quotient.substring(repeatingDividend.get(dividend), quotient.length()) + ")";
+            }
+            repeatingDividend.put(dividend, quotient.length());
+        }
+        return quotient.toString();
+    }
+}
