11ea4376f05c60b77b49dc9a9062b7ca87e94808
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-09-27 03:11:27 +0530

    Time: 183 ms (14.12%), Space: 43.6 MB (38.37%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -4,8 +4,8 @@ class Solution {
         int n = nums.length, count = 0;
         for (int i = 0; i < n - 2; i++) {
             for (int j = i + 1; j < n - 1; j++) {
-                int lastValidIdx = binarySearch(j, n, nums[i] + nums[j], nums);
-                // System.out.println(i +"\t"+ j + "\t" + lastValidIdx);
+                int lastValidIdx = binarySearch(j, n - 1, nums[i] + nums[j], nums);
+                // System.out.println(i + "\t" + j + "\t" + lastValidIdx);
                 count += lastValidIdx - j;
             }
         }
@@ -14,7 +14,7 @@ class Solution {
 
     private int binarySearch(int lt, int rt, int target, int[] nums) {
         int idx = lt;
-        while (lt < rt) {
+        while (lt <= rt) {
             int mid = (lt + rt) / 2;
             if (nums[mid] >= target)
                 rt = mid - 1;
