a547502c4e45ea0337ea4767438d78f0b5d81cee
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-09-28 19:35:44 +0530

    Time: 8 ms (52.54%), Space: 45.5 MB (17.04%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
@@ -3,11 +3,8 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         for (int i = n - 1; i >= 2; i--) {
-            for (int j = i - 1; j >= 1; j--) {
-                int k = j - 1;
-                while (k >= 0 && nums[i] < nums[j] + nums[k] && nums[k] != 0)
-                    return nums[i] + nums[j] + nums[k--];
-            }
+            if (nums[i] < nums[i - 1] + nums[i - 2])
+                return nums[i] + nums[i - 1] + nums[i - 2];
         }
         return 0;
     }
