1a7a967c9e7fcd3ac89e2011c078fe736af25941
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-10-02 20:51:14 +0530

    Time: 0 ms (100%), Space: 40.7 MB (91.61%) - LeetHub

---
+++ 3100-water-bottles-ii/3100-water-bottles-ii.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int ans = 0;
+        while (numBottles++ >= numExchange) {
+            ans += numExchange;
+            numBottles -= numExchange++;
+        }
+        return ans += --numBottles;
+    }
+}
