6f692cfcde54dbd4c01cf705f35c581a533cf02f
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-10-16 03:53:38 +0530

    Time: 13 ms (99.13%), Space: 88.9 MB (32.9%) - LeetHub

--- 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.java
+++ 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.java
@@ -2,17 +2,17 @@ class Solution {
     public int maxIncreasingSubarrays(List<Integer> nums) {
         int n = nums.size(), prev = 0, curr = 1, ans = 0;
         for (int i = 1; i < n; i++) {
-            if (nums.get(i) > nums.get(i - 1))
+            if (nums.get(i) > nums.get(i - 1)) // incr curr streak
                 curr++;
-            else {
-                ans = Math.max(ans, Math.min(prev, curr));
-                ans = Math.max(ans, prev / 2);
+            else { // reset streak after recording stats
+                ans = Math.max(ans, Math.min(prev, curr)); // <-|-> Take only valid prev & curr window
+                ans = Math.max(ans, prev / 2); // also consider that prev window could be split into 2 adj ones
                 prev = curr;
                 curr = 1;
             }
         }
-        ans = Math.max(ans, Math.min(prev, curr));
-        ans = Math.max(ans, Math.max(prev, curr) / 2);
+        ans = Math.max(ans, Math.min(prev, curr)); // account for last running streaks (curr & prev)
+        ans = Math.max(ans, Math.max(prev, curr) / 2); // account for split of last running streaks
         return ans;
     }
 }
