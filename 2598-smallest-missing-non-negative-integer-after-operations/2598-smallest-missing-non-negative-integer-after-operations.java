693069dd7dd454cc06a4b04e12c1ccf11a614d73
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-10-17 02:28:18 +0530

    Time: 6 ms (84.42%), Space: 66.4 MB (5.03%) - LeetHub

--- 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.java
+++ 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.java
@@ -9,11 +9,9 @@ class Solution {
                 freq[(value - (Math.abs(nums[i]) % value)) % value]++;
         // System.out.println(Arrays.toString(freq));
         for (int i = 0; i < nums.length; i++) {
-            if (freq[i] > 0)
-                continue;
-            else if (freq[i % value] > 1) {
+            if (freq[i % value] >= 1) {
                 freq[i % value]--; // carry over 1 entry from mod bucket
-                freq[i] = 1;
+                // freq[i] = 1;     // not necessary for calc., leaving here for intuition
             } else
                 return i;
         }
