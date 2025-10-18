72c6dd7e415593d5a952ab6dea6ae5bf16eb868d
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-10-19 04:57:36 +0530

    Time: 19 ms (85.03%), Space: 58.4 MB (44.9%) - LeetHub

--- 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.java
+++ 3397-maximum-number-of-distinct-elements-after-operations/3397-maximum-number-of-distinct-elements-after-operations.java
@@ -1,12 +1,13 @@
 class Solution {
     public int maxDistinctElements(int[] nums, int k) {
         Arrays.sort(nums);
-        TreeSet<Integer> set = new TreeSet<>();
-        set.add(Integer.MIN_VALUE);
-        for (int n : nums)
-            set.add(Math.min(Math.max(set.last() + 1, n - k), n + k));
-        set.remove(Integer.MIN_VALUE);
-        // System.out.println(set);
-        return set.size();
+        int prev = Integer.MIN_VALUE, distinct = 0;
+        for (int n : nums) {
+            if (prev == n + k)
+                continue;
+            prev = Math.min(Math.max(prev + 1, n - k), n + k);
+            distinct++;
+        }
+        return distinct;
     }
 }
