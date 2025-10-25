f159808ef113ebddd2feffb9b925fe019294782a
Suvraneel Bhuin <bsuvraneel@gmail.com>
2025-10-26 04:00:14 +0530

    Time: 0 ms (100%), Space: 40.9 MB (9.25%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -2,8 +2,9 @@ class Solution {
     public int totalMoney(int n) {
         int weeks = n / 7, days = n % 7;
         // ans = AP for complete weeks + AP for remaining days
+        // ans = [28 + 35 + ...weeks] + [pattern at start of last week + ...days]
         // ans = [7 * (4 + 5 + 6 + ...weeks)] + [(1+weeks) + (2+weeks) + ...days]
-        // ans = 7 * Sum of AP, Sn = 7 * n/2 * 2a + (n-1) * d/
-        return (int)((7 * (8 + (weeks - 1)) * (weeks / 2f)) + ((2 * (1 + weeks) + (days - 1)) * (days / 2f)));
+        // Use in both : Sum of AP, Sn = (2a + (n-1) * d) * n/2
+        return (int) ((7 * (8 + (weeks - 1)) * (weeks / 2f)) + ((2 * (1 + weeks) + (days - 1)) * (days / 2f)));
     }
 }
