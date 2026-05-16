1c2886721060967b751293d406ef3e71b863b986
Suvraneel Bhuin <bsuvraneel@gmail.com>
2026-05-17 05:07:38 +0530

    Time: 19 ms (80.92%), Space: 46 MB (30.89%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,27 +1,25 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            if (i != 0 && nums[i] == nums[i - 1])
+            if (i > 0 && nums[i] == nums[i - 1]) // skip dup
                 continue;
             for (int j = i + 1; j < n; j++) {
-                if (j != i + 1 && nums[j] == nums[j - 1])
+                if (j > i + 1 && nums[j] == nums[j - 1]) // skip dup
                     continue;
-                int k = j + 1, l = n - 1;
-                long rem = (long) target - (nums[i] + nums[j]);
-                while (k < l) {
-                    long sum = nums[k] + nums[l];
-                    if (rem == sum) {
+                for (int k = j + 1, l = n - 1; k < l;) { // 2 ptr
+                    long sum = 0l + nums[i] + nums[j] + nums[k] + nums[l];
+                    if (sum == target) {
                         ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
                         k++;
                         l--;
-                        while (k < l && nums[k] == nums[k - 1])
+                        while (k < l && nums[k] == nums[k - 1]) // skip dup
                             k++;
-                        while (k < l && nums[l] == nums[l + 1])
+                        while (k < l && nums[l] == nums[l + 1]) // skip dup
                             l--;
-                    } else if (rem > sum)
+                    } else if (sum < target)
                         k++;
                     else
                         l--;
