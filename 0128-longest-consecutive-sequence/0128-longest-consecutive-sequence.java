a170284095ac0d471a1fa250e56d45715e2457b5
Suvraneel Bhuin <bsuvraneel@gmail.com>
2026-05-17 01:58:10 +0530

    Time: 208 ms (5.04%), Space: 138.4 MB (5.01%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,19 +1,70 @@
 class Solution {
-    public int longestConsecutive(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length, streak = 1, ans = 0;
-        if (n == 0)
-            return 0;
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i + 1] == nums[i])
-                ;
-            else if (nums[i + 1] - nums[i] == 1)
-                streak++;
-            else {
-                ans = Math.max(ans, streak);
-                streak = 1;
+    private class UnionFind {
+        Map<Integer, Integer> root = new HashMap<>();
+        Map<Integer, Integer> rank = new HashMap<>();
+
+        UnionFind(int[] nums) {
+            for (int i : nums) {
+                root.put(i, i);
+                rank.put(i, 1);
             }
         }
-        return Math.max(ans, streak);
+
+        private int find(int x) {
+            int rootX = root.get(x);
+            if (rootX == x)
+                return x;
+            int head = find(rootX);
+            root.put(x, head);
+            return head;
+        }
+
+        private void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                int rankX = rank.get(x), rankY = rank.get(y);
+                if (rankX < rankY)
+                    root.put(rootY, rootX);
+                else if (rankX > rankY)
+                    root.put(rootX, rootY);
+                else {
+                    root.put(rootY, rootX);
+                    rank.put(rootX, rank.get(rootX) + 1);
+                }
+            }
+        }
+
+        private boolean connected(int x, int y) {
+            return find(x) == find(y);
+        }
+
+        private boolean validKey(int x) {
+            return root.containsKey(x);
+        }
+
+        private int getMaxFreq() {
+            Map<Integer, Integer> freq = new HashMap<>();
+            for (Integer key : root.keySet()) {
+                int head = find(key);
+                freq.put(head, freq.getOrDefault(head, 0) + 1);
+            }
+            int ans = 0;
+            for (Integer value : freq.values())
+                ans = Math.max(ans, value);
+            return ans;
+        }
+    }
+
+    public int longestConsecutive(int[] nums) {
+        UnionFind uf = new UnionFind(nums);
+        for (int i : nums) {
+            if (uf.validKey(i - 1))
+                uf.union(i, i - 1);
+            if (uf.validKey(i + 1))
+                uf.union(i, i + 1);
+        }
+        // System.out.println(uf.root);
+        return uf.getMaxFreq();
     }
 }
