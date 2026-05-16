d7fd58b337489ffe03f53f9ca4536c71327f424a
Suvraneel Bhuin <bsuvraneel@gmail.com>
2026-05-17 04:58:13 +0530

    Time: 33 ms (74.01%), Space: 58.9 MB (89.98%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,17 +4,17 @@ class Solution {
         int n = nums.length;
         Arrays.sort(nums);
         for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
+            if (i > 0 && nums[i] == nums[i - 1]) // skip dup
                 continue;
-            for (int j = i + 1, k = n - 1; j < k;) {
+            for (int j = i + 1, k = n - 1; j < k;) { // 2 ptr
                 int sum = nums[i] + nums[j] + nums[k];
                 if (sum == 0) {
                     ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     j++;
                     k--;
-                    while (j < k && nums[j] == nums[j - 1])
+                    while (j < k && nums[j] == nums[j - 1]) // skip dup
                         j++;
-                    while (j < k && nums[k] == nums[k + 1])
+                    while (j < k && nums[k] == nums[k + 1]) // skip dup
                         k--;
                 } else if (sum < 0)
                     j++;
