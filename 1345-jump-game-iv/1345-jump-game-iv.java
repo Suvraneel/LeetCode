564e5974660c300e1d01f8021f1dffe3aa6a29aa
Suvraneel Bhuin <bsuvraneel@gmail.com>
2026-05-19 01:46:19 +0530

    Time: 67 ms (26.73%), Space: 110.2 MB (6.34%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.java
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -1,37 +1,72 @@
 class Solution {
-    public int minJumps(int[] arr) {
-        int n = arr.length, ans = 0;
+    public static int minJumps(int[] arr) {
+        int n = arr.length;
+        if (n <= 1) {
+            return 0;
+        }
+
         Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int i = 0; i < n; i++)
-            graph.computeIfAbsent(arr[i], v -> new ArrayList<>()).add(i);
-        boolean[] vis = new boolean[n];
-        Queue<Integer> level = new LinkedList<>(Arrays.asList(0));
-        while (!level.isEmpty()) {
-            int size = level.size();
-            while (size-- > 0) {
-                int node = level.poll();
-                if (node == n - 1)
-                    return ans;
-                // Check same valued jumps
-                for (int nbr : graph.get(arr[node])) {
-                    if (!vis[nbr]) {
-                        level.offer(nbr);
-                        vis[nbr] = true;
+        for (int i = 0; i < n; i++) {
+            graph.computeIfAbsent(arr[i], v -> new LinkedList<>()).add(i);
+        }
+
+        HashSet<Integer> curs = new HashSet<>(); // store layers from start
+        curs.add(0);
+        Set<Integer> visited = new HashSet<>();
+        visited.add(0);
+        visited.add(n - 1);
+        int step = 0;
+
+        HashSet<Integer> other = new HashSet<>(); // store layers from end
+        other.add(n - 1);
+
+        // when current layer exists
+        while (!curs.isEmpty()) {
+            // search from the side with fewer nodes
+            if (curs.size() > other.size()) {
+                HashSet<Integer> tmp = curs;
+                curs = other;
+                other = tmp;
+            }
+
+            HashSet<Integer> nex = new HashSet<>();
+
+            // iterate the layer
+            for (int node : curs) {
+
+                // check same value
+                for (int child : graph.get(arr[node])) {
+                    if (other.contains(child)) {
+                        return step + 1;
+                    }
+                    if (!visited.contains(child)) {
+                        visited.add(child);
+                        nex.add(child);
                     }
                 }
+
+                // clear the list to prevent redundant search
                 graph.get(arr[node]).clear();
-                // Check adjacent jumps
-                if (0 < node && !vis[node - 1]) {
-                    level.offer(node - 1);
-                    vis[node - 1] = true;
+
+                // check neighbors
+                if (other.contains(node + 1) || other.contains(node - 1)) {
+                    return step + 1;
                 }
-                if (node < n - 1 && !vis[node + 1]) {
-                    level.offer(node + 1);
-                    vis[node + 1] = true;
+
+                if (node + 1 < n && !visited.contains(node + 1)) {
+                    visited.add(node + 1);
+                    nex.add(node + 1);
+                }
+                if (node - 1 >= 0 && !visited.contains(node - 1)) {
+                    visited.add(node - 1);
+                    nex.add(node - 1);
                 }
             }
-            ans++;
+
+            curs = nex;
+            step++;
         }
+
         return -1;
     }
 }
