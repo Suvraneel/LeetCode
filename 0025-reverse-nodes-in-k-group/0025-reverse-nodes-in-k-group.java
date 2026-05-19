3b158aa64171b69c5fc65024b34a2bdd5216d1ae
Suvraneel Bhuin <bsuvraneel@gmail.com>
2026-05-20 04:55:28 +0530

    Time: 0 ms (100%), Space: 46.3 MB (54.39%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -21,16 +21,19 @@ class Solution {
                 i++;
             }
             if (i == k) {
-                prev.next = null;
-                newChain = reverseLL(kHead);
-                lastChain[1].next = newChain[0]; //lastChain tail -> newChain head
+                prev.next = null; // disconnect next chain
+                newChain = reverseLL(kHead); // returns {head, tail} after reversal
+                lastChain[1].next = newChain[0]; // connect lastChain tail -> newChain head
                 lastChain = newChain;
             } else
-                lastChain[1].next = kHead;
+                lastChain[1].next = kHead; // connect without reversal of remnants
         }
         return dummy.next;
     }
 
+    /** Reverse a LinkedList
+    * @return ListNode[]{head, tail}
+    */
     private ListNode[] reverseLL(ListNode head) {
         ListNode prev = null, curr = head;
         while (curr != null) {
