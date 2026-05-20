d916f6e9acf169a1e625983fb197b15181846fcc
Suvraneel Bhuin <bsuvraneel@gmail.com>
2026-05-20 22:28:50 +0530

    Time: 145 ms (44.59%), Space: 42.9 MB (75.04%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -5,30 +5,31 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         this.m = board.length;
         this.n = board[0].length;
+        boolean[][] vis = new boolean[m][n];
         boolean ans = false;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] != word.charAt(0))
                     continue;
-                board[i][j] = '#';
-                ans |= solve(board, word, 1, i, j);
-                board[i][j] = word.charAt(0);
+                vis[i][j] = true;
+                ans |= solve(board, word, 1, i, j, vis);
+                vis[i][j] = false;
             }
         }
         return ans;
     }
 
-    boolean solve(char[][] board, String word, int idx, int x, int y) {
+    boolean solve(char[][] board, String word, int idx, int x, int y, boolean[][] vis) {
         if (idx == word.length())
             return true;
         boolean ans = false;
         for (int[] d : dirs) {
             int X = x + d[0], Y = y + d[1];
-            if (X < 0 || X >= m || Y < 0 || Y >= n || word.charAt(idx) != board[X][Y])
+            if (X < 0 || X >= m || Y < 0 || Y >= n || vis[X][Y] || word.charAt(idx) != board[X][Y])
                 continue;
-            board[X][Y] = '#';
-            ans |= solve(board, word, idx + 1, X, Y);
-            board[X][Y] = word.charAt(idx);
+            vis[X][Y] = true;
+            ans |= solve(board, word, idx + 1, X, Y, vis);
+            vis[X][Y] = false;
         }
         return ans;
     }
