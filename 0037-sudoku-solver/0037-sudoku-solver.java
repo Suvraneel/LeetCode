2ca4cf082e6894d8fa855164efdd0cae3a1c753c
Suvraneel Bhuin <bsuvraneel@gmail.com>
2026-05-21 01:58:05 +0530

    Time: 326 ms (47.27%), Space: 42.8 MB (69.82%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,42 +1,35 @@
 class Solution {
-    int m = 9, n = 9;
+    char[][] ans = new char[9][9];
 
     public void solveSudoku(char[][] board) {
-        solve(board);
+        solve(board, 0, 0);
     }
 
-    private boolean solve(char[][] board) {
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (board[i][j] == '.') {
-                    for (char k = '1'; k <= '9'; k++)
-                        if (isValid(i, j, k, board)) {
-                            board[i][j] = k;
-                            if (solve(board))
-                                return true;
-                            board[i][j] = '.';
-                        }
-                    return false; // dead end, backtrack
-                }
-        return true; // Constraint: It is guaranteed that the input board has only one solution.
-    }
+    boolean solve(char[][] board, int i, int j) {
+        if (i == 9)
+            return true;
 
-    private boolean isValid(int r, int c, char k, char[][] board) {
-        for (int i = 0; i < m; i++) {
-            if (board[i][c] == k)
-                return false;
-        }
-        for (int j = 0; j < n; j++) {
-            if (board[r][j] == k)
-                return false;
-        }
-        int R = (int) (r / 3) * 3, C = (int) (c / 3) * 3;
-        for (int i = R; i < R + 3; i++) {
-            for (int j = C; j < C + 3; j++) {
-                if (board[i][j] == k)
-                    return false;
+        if (j == 9)
+            return solve(board, i + 1, 0);
+
+        if (board[i][j] != '.')
+            return solve(board, i, j + 1);
+
+        for (char d = '1'; d <= '9'; d++) {
+            if (isSafe(board, i, j, d)) {
+                board[i][j] = d;
+                if (solve(board, i, j + 1))
+                    return true;
+                board[i][j] = '.';
             }
         }
+        return false;
+    }
+
+    boolean isSafe(char[][] board, int i, int j, char d) {
+        for (int k = 0; k < 9; k++)
+            if (board[k][j] == d || board[i][k] == d || board[3 * (i / 3) + k / 3][3 * (j / 3) + k % 3] == d)
+                return false;
         return true;
     }
 }
