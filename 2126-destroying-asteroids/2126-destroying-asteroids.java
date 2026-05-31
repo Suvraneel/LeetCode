c2a8b50162a3f4a9968a22e4b6aa8aacc9c1a6ce
Suvraneel Bhuin <bsuvraneel@gmail.com>
2026-06-01 05:14:51 +0530

    Time: 26 ms (81.65%), Space: 107.6 MB (16.47%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -2,6 +2,8 @@ class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
         Arrays.sort(asteroids);
         long m = mass;
+        if (m > asteroids[asteroids.length - 1])
+            return true;
         for (int i : asteroids) {
             if (m < i)
                 return false;
