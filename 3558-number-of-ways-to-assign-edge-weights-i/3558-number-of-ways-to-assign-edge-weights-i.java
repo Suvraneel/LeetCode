cbcd5b3866ca2d916186b470114d093411cb9134
Suvraneel Bhuin <bsuvraneel@gmail.com>
2026-06-12 04:31:35 +0530

    Time: 168 ms (28.57%), Space: 301 MB (19.05%) - LeetHub

--- 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.java
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.java
@@ -6,13 +6,12 @@ class Solution {
             adj.computeIfAbsent(e[0], k -> new ArrayList<>()).add(e[1]);
             adj.computeIfAbsent(e[1], k -> new ArrayList<>()).add(e[0]);
         }
-        // Find max depth
+        // Find max depth via dfs
         int maxDepth = dfs(1, adj, 0);
-        int MOD = 1_000_000_007;
-        int ans = 1;
-        while (--maxDepth > 0)
-            ans = (ans * 2) % MOD;
-        return ans;
+        // Calc. odd parity of 1 weighted edges, rest 2, viz - nC1+nC3+nC5+...nCn_or_n-1
+        // Above series equates to 2^(n-1) via binary expansion
+        // Now instead of pow(2, n-1)%10e7, we will use fast exponentiation
+        return fastExponentiation(2, maxDepth - 1);
     }
 
     int dfs(int node, Map<Integer, List<Integer>> adj, int parent) {
@@ -25,5 +24,19 @@ class Solution {
             maxDepth = Math.max(maxDepth, dfs(child, adj, node) + 1);
         }
         return maxDepth;
+    }
+
+    int MOD = 1_000_000_007;
+
+    int fastExponentiation(int x, int y) { // calc x^n % 10e7
+        long ans = 1;
+        long base = x;
+        while (y > 0) {
+            if ((y & 1) == 1)
+                ans = (ans * base) % MOD;
+            base = (base * base) % MOD;
+            y >>= 1;
+        }
+        return (int) ans;
     }
 }
