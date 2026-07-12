d7acfe32a071509e3008974107e3026714483f20
Suvraneel Bhuin <bsuvraneel@gmail.com>
2026-07-12 06:19:27 +0530

    Time: 34 ms (40.53%), Space: 77.2 MB (20.51%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -2,20 +2,19 @@ class Solution {
     public int[] arrayRankTransform(int[] arr) {
         if (arr.length == 0)
             return new int[0];
-        Queue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        for (int i = 0; i < arr.length; i++)
-            pq.offer(new int[] { arr[i], i });
-        int idx = 0, prev = pq.peek()[0] - 1;
+        int[] clone = arr.clone();
+        Arrays.sort(clone);
+        int idx = 0;
+        Map<Integer, Integer> m = new HashMap<>();
+        m.put(clone[0], ++idx);
+        for (int i = 1; i < clone.length; i++)
+            if (clone[i] != clone[i - 1])
+                m.put(clone[i], ++idx);
+            else
+                m.put(clone[i], idx);
         int[] ans = new int[arr.length];
-        while (!pq.isEmpty()) {
-            int[] top = pq.poll();
-            if (prev == top[0])
-                ans[top[1]] = idx;
-            else {
-                ans[top[1]] = ++idx;
-                prev = top[0];
-            }
-        }
+        for (int i = 0; i < arr.length; i++)
+            ans[i] = m.get(arr[i]);
         return ans;
     }
 }
