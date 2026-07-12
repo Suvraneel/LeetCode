1e99441f78ded5bc7442e9a9c845a6be223c31a9
Suvraneel Bhuin <bsuvraneel@gmail.com>
2026-07-12 06:08:06 +0530

    Time: 53 ms (14.69%), Space: 69.7 MB (94.83%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int n = arr.length;
-        Map<Integer, List<Integer>> posn = new TreeMap<>();
-        for(int i=0; i<n; i++){
-            posn.putIfAbsent(arr[i], new ArrayList<>());
-            List<Integer> sameRankIndexes = posn.get(arr[i]);
-            sameRankIndexes.add(i);
-            posn.put(arr[i], sameRankIndexes);
-        }
-        int[] ans = new int[n];
-        int rank = 1;
-        for(Map.Entry<Integer, List<Integer>> e: posn.entrySet()){
-            List<Integer> sameRankIndexes = e.getValue();
-            for(int i: sameRankIndexes)
-                ans[i] = rank;
-            rank++;
+        if (arr.length == 0)
+            return new int[0];
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        for (int i = 0; i < arr.length; i++)
+            pq.offer(new int[] { arr[i], i });
+        int idx = 0, prev = pq.peek()[0] - 1;
+        int[] ans = new int[arr.length];
+        while (!pq.isEmpty()) {
+            int[] top = pq.poll();
+            if (prev == top[0])
+                ans[top[1]] = idx;
+            else {
+                ans[top[1]] = ++idx;
+                prev = top[0];
+            }
         }
         return ans;
     }
