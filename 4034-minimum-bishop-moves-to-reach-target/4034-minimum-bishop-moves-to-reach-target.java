734a919bd977f6d078ca6bfd1147398122a50a21
Suvraneel Bhuin <bsuvraneel@gmail.com>
2026-08-30 02:44:02 +0530

    Time: 1 ms (100%), Space: 43.7 MB (100%) - LeetHub

--- 4034-minimum-bishop-moves-to-reach-target/4034-minimum-bishop-moves-to-reach-target.java
+++ 4034-minimum-bishop-moves-to-reach-target/4034-minimum-bishop-moves-to-reach-target.java
@@ -3,7 +3,7 @@ class Solution {
         int sr = source[0], sc = source[1], tr = target[0], tc = target[1];
         if ((sr + sc) % 2 != (tr + tc) % 2) // parity mismatch => opp. colored squares
             return -1;
-        if (Math.abs(1.0 * (tc - sc) / (tr - sr)) == 1)
+        if (Math.abs(1.0 * (tc - sc) / (tr - sr)) == 1) // slope = n*pi/2 => same diagonal
             return 1;
         return 2;
     }
