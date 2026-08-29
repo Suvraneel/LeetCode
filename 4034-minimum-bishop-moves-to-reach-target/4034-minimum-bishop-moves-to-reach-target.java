358407c10a174835cd74375ab0b7fd5b623e5bb6
Suvraneel Bhuin <bsuvraneel@gmail.com>
2026-08-30 02:43:14 +0530

    Time: 1 ms (100%), Space: 44.2 MB (0%) - LeetHub

---
+++ 4034-minimum-bishop-moves-to-reach-target/4034-minimum-bishop-moves-to-reach-target.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minBishopMoves(int[] source, int[] target) {
+        int sr = source[0], sc = source[1], tr = target[0], tc = target[1];
+        if ((sr + sc) % 2 != (tr + tc) % 2) // parity mismatch => opp. colored squares
+            return -1;
+        if (Math.abs(1.0 * (tc - sc) / (tr - sr)) == 1)
+            return 1;
+        return 2;
+    }
+}
