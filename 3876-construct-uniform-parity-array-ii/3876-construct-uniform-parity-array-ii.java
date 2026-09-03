cce72a8c7ac2d103252dc23185f43af53896194a
Suvraneel Bhuin <bsuvraneel@gmail.com>
2026-09-04 04:38:26 +0530

    Time: 3 ms (90.06%), Space: 121.9 MB (24.72%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -3,18 +3,14 @@ class Solution {
         // Intuition: The minimum element in array must be odd,
         // that way it can reverse the parity of other even/odd elements
         // if its not odd, only viable condition is if entire array is even elems.
-        int odd = 0, even = 0, min = Integer.MAX_VALUE;
+        int odd = 0, min = Integer.MAX_VALUE;
         for (int i : nums1) {
             if ((i & 1) == 1)
                 odd++;
-            else
-                even++;
             min = Math.min(min, i);
         }
         if ((min & 1) == 1)
             return true;
-        else if (odd == 0)
-            return true;
-        return false;
+        return odd == 0;
     }
 }
