cf2d179480dc758fa7592b6c046a14d71e47e628
Suvraneel Bhuin <bsuvraneel@gmail.com>
2026-09-06 05:43:47 +0530

    Time: 5 ms (50.78%), Space: 133.1 MB (36.95%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -1,18 +1,15 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        int n = nums.length;
-        int[] prefMax = new int[n];
-        int[] suffMin = new int[n];
-        prefMax[0] = nums[0];
-        suffMin[n - 1] = nums[n - 1];
-        for (int i = 1; i < n; i++) {
-            prefMax[i] = Math.max(prefMax[i - 1], nums[i]);
-            suffMin[n - 1 - i] = Math.min(suffMin[n - i], nums[n - 1 - i]);
+        int n = nums.length, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        int[] instability = new int[n];
+        for (int i = 0; i < n; i++) {
+            instability[i] += max = Math.max(max, nums[i]);
+            instability[n - 1 - i] -= min = Math.min(min, nums[n - 1 - i]);
         }
         // System.out.println(Arrays.toString(prefMax));
         // System.out.println(Arrays.toString(suffMin));
         for (int i = 0; i < n; i++)
-            if (prefMax[i] - suffMin[i] <= k)
+            if (instability[i] <= k)
                 return i;
         return -1;
     }
